ed7162c05444788f3634a112c30343e0e649894f
yeswanthannadata <annadatayeswanth@gmail.com>
2017-05-10 06:06:01 -0400

    Adding beta settings

--- ANGULAR_WMS/app/scripts/extentions/auth/session.js
+++ ANGULAR_WMS/app/scripts/extentions/auth/session.js
@@ -4,8 +4,8 @@
   angular.module("auth").service("Session", function ($rootScope, $q) {
 
     var that = this;
-    that.host = 'https://api.stockone.in/';
-    //that.host = 'http://dev.stockone.in/';
+    //that.host = 'https://api.stockone.in/';
+    that.host = 'http://dev.stockone.in/';
     //that.host = 'http://176.9.181.43:7654/';
     that.url = that.host+'rest_api/';
 

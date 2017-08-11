67a3c174ac3aa74fedb89dea5b60e2c59bb0b427
sree522 <sreekanth.t1994@gmail.com>
2017-08-11 02:23:49 -0400

    Adding shipment info modifications

--- API_WMS/miebach/rest_api/views/miebach_utils.py
+++ API_WMS/miebach/rest_api/views/miebach_utils.py
@@ -904,7 +904,8 @@ SUMMARY_INTER_STATE_STATUS = {0: 'intra_state', 1: 'inter_state', '2': 'default'
 
 GSTIN_USER_MAPPING = {'sagar_fab': '29ABEFS4899J1ZA', 'adam_clothing1': '2788OFB3466F1ZB', 'adam_abstract': '2788OFB3466F1ZB',
                       'dazzle_export': '26AHQPP2057B1ZB', 'legends_overseas': '27AAGFL3290D1ZF', 'TranceHomeLinen': '29ADOPS6189BIZX',
-                      'demo': 'ABC12345678', 'sjpmg': '07BDBPS8474F1Z7', 'tshirt_inc': '36AAHFT9169L1ZC'}
+                      'demo': 'ABC12345678', 'sjpmg': '07BDBPS8474F1Z7', 'tshirt_inc': '36AAHFT9169L1ZC',
+                      'scholar_clothing': '29AMPPN7507A1ZW', 'campus_sutra': '29AAIFC4655P1ZQ'}
 
 #End of Username and GST Tin Mapping
 

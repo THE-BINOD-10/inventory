375b48a8598ffc4bde57311145c76f164e506216
hy5man <smstoraj@gmail.com>
2017-12-01 14:16:05 +0530

    Tally API

--- API_WMS/miebach/rest_api/views/tally.py
+++ API_WMS/miebach/rest_api/views/tally.py
@@ -24,24 +24,30 @@ class TallyAPI:
         self.headers = { 'ContentType' : self.content_type }
     
     def get_item_master(self, limit=10):
+        limit = 10
         send_ids = []
-        user_id = 1
         exclude_ids = OrdersAPI.objects.filter(user=user_id, engine_type='Tally', order_type='sku',\
             status__in=[1,9]).values_list('order_id', flat=True)
-        sku_masters = SKUMaster.objects.exclude(id__in=exclude_ids).filter(user=user_id)[:10]
+        sku_masters = SKUMaster.objects.exclude(id__in=exclude_ids).filter(user=user_id)[:limit]
         tally_company_name = 'Mieone'
         data_list = []
         for sku_master in sku_masters:
             data_dict = {}
+            data_dict['tally_company_name'] = self.tally_dict.get('company_name', '')
+            data_dict['old_item_name'] = ''
             data_dict['item_name'] = sku_master.sku_desc
-            data_dict['sku_code'] = sku_master.sku_code
-            data_dict['description'] = sku_master.sku_desc
-            data_dict['unit_name'] = sku_master.measurement_type
+            data_dict['item_alias'] = ''
+            data_dict['primaryUnitName'] = ''
             data_dict['stock_group_name'] = self.tally_dict.get('stock_group', '')
             data_dict['stock_category_name'] = self.tally_dict.get('stock_category', '')
-            data_dict['opening_rate'] = sku_master.price
+            data_dict['isVatAppl'] = ''
+            data_dict['openingQty'] = ''
+            data_dict['openingRate'] = sku_master.price
             data_dict['opening_amt'] = 0
-            data_dict['tally_company_name'] = self.tally_dict.get('company_name', '')
+            data_dict['part_no'] = ''
+            data_dict['description'] = sku_master.sku_desc
+            data_dict['sku_code'] = sku_master.sku_code
+            data_dict['unit_name'] = sku_master.measurement_type
             data_list.append(data_dict)
         return HttpResponse(json.dumps(data_list))
 
@@ -51,30 +57,36 @@ class TallyAPI:
         data_list = []
         for master in masters:
             data_dict = {}
-            data_dict['tally_company_name'] = self.tally_dict.get('company_name', '')
-            data_dict['ledger_name'] = master.name
-            data_dict['ledger_alias'] = getattr(master, field_mapping['id'])
+            data_dict['tallyCompanyName'] = self.tally_dict.get('company_name', '')
+            data_dict['oldLedgerName'] = ''
+            data_dict['ledgerName'] = master.name
+            data_dict['ledgerAlias'] = getattr(master, field_mapping['id'])
+            data_dict['updateOpeningBalance'] = getattr(master, field_mapping['id'])
+            data_dict['openingBalance'] = 'Optional'
             parent_group_name = ''
             master_type = getattr(master, field_mapping['type'])
             group_obj = master_group.filter(master_value=master_type)
             if group_obj:
                 parent_group_name = group_obj[0].parent_group
-            data_dict['parentGroupName'] = parent_group_name
             data_dict['ledgerMailingName'] = master.name
-            data_dict['address_1'] = master.address
+            data_dict['parentGroupName'] = parent_group_name
+            data_dict['address'] = master.address
             data_dict['state'] = master.state
-            data_dict['pin_code'] = master.pincode
+            data_dict['pinCode'] = master.pincode
             data_dict['country'] = master.country
-            data_dict['telephone_no'] = master.phone_number
+            data_dict['contactPerson'] = ''
+            data_dict['telephoneNo'] = master.phone_number
+            data_dict['faxNo'] = master.phone_number
             data_dict['email'] = master.email_id
-            data_dict['tin_no'] = master.tin_number
-            data_dict['cst_no'] = master.cst_number
-            data_dict['pan_no'] = master.pan_number
+            data_dict['tinNo'] = master.tin_number
+            data_dict['cstNo'] = master.cst_number
+            data_dict['panNo'] = master.pan_number
+            data_dict['serviceTaxNo'] = ''
             if master_type == 'customer':
                 credit_period = master.credit_period
                 if not credit_period and self.tally_dict.get('credit_perod', 0):
                     credit_period = self.tally_dict.get('credit_perod')
-                data_dict['default_credit_period'] = credit_period
+                data_dict['defaultCreditPeriod'] = credit_period
             data_dict['maintainBillWiseDetails'] = STATUS_DICT[self.tally_dict.get('maintain_bill', 0)]
             data_list.append(data_dict)
         return data_list

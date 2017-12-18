1ec77a341f842f20440cf3fc8105622cdb77cf68
sujith <sujiththedla050@gmail.com>
2017-12-18 15:24:51 +0530

    change the endpoint

--- POS/sw.js
+++ POS/sw.js
@@ -8,7 +8,7 @@ importScripts('/app/data/offlineData.js');
 	"use strict";
 	//service worker version number
 
-	var VERSION="0.0.0.91-build-0.9.0.50"
+	var VERSION="0.0.0.91-build-0.9.0.53"
 
 	//service worker version name
 	var CACHE_NAME="POS"+VERSION;

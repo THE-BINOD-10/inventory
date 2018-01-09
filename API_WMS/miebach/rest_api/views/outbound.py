2585a2d5acc753b45b8de4319281d161ab7a8a35
saipavan0210 <sai@headrun.com>
2018-01-09 11:01:54 +0530

    Picked Quantity Status display in Your Orders Page

--- API_WMS/miebach/rest_api/views/outbound.py
+++ API_WMS/miebach/rest_api/views/outbound.py
@@ -6014,8 +6014,9 @@ def get_level_based_customer_orders(request, response_data, user=''):
     else:
         cum_obj = CustomerUserMapping.objects.filter(user=request.user.id)
     if cum_obj:
+        customer_id = cum_obj[0].customer.customer_id
         cm_id = cum_obj[0].customer_id
-        picklist = Picklist.objects.filter(order__customer_id=cm_id, order__user=user.id)
+        picklist = Picklist.objects.filter(order__customer_id=customer_id, order__user=user.id)
         generic_orders = GenericOrderDetailMapping.objects.filter(customer_id=cm_id)
         response_data['data'] = list(generic_orders.values('generic_order_id', 'customer_id'). \
                                      annotate(total_quantity=Sum('quantity'),
@@ -6031,11 +6032,11 @@ def get_level_based_customer_orders(request, response_data, user=''):
                 status = 'open'
             else:
                 status = 'closed'
-                pick_status = picklist.filter(order__order_id__in=order_detail_ids,
+                pick_status = picklist.filter(order_id__in=order_detail_ids,
                                               status__icontains='open')
                 if pick_status:
                     status = 'open'
-            picked_quantity = picklist.filter(order__order_id__in=order_detail_ids).aggregate(
+            picked_quantity = picklist.filter(order_id__in=order_detail_ids).aggregate(
                 Sum('picked_quantity'))['picked_quantity__sum']
             if not picked_quantity:
                 picked_quantity = 0

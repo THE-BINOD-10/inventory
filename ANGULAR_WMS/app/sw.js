8659da9934673e1ab6705c82a45645d23562be33
sree522 <sreekanth.t1994@gmail.com>
2018-01-12 18:55:38 +0100

    Adding prod configurations

--- ANGULAR_WMS/app/sw.js
+++ ANGULAR_WMS/app/sw.js
@@ -6,7 +6,7 @@ self.importScripts('./scripts/App/offlineDB.js');
    "use strict";
 
 
- var version        = "0.0.1-build03.0.117",
+ var version        = "0.0.1-build03.0.139",
       cacheName     = "stock1-app-" + version,
       Directory     ="";
  var APICALL        ="https://api.stockone.in/rest_api/";

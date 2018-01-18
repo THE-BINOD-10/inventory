4b960e3768108aa65a23e567c4c2659ae7a15f75
hy5man <smstoraj@gmail.com>
2018-01-18 18:29:57 +0530

    Payment Type - POS

---
+++ POS/app/payment_type/paymenttype.component.js
@@ -0,0 +1,13 @@
+;(function (angular) {
+  "use strict";
+
+  angular.module("paymenttype", [])
+         .component("paymenttype", {
+           "templateUrl": "/app/payment_type/paymenttype.template.html",
+           "controller"  : ["$http", "$scope", "urlService", "manageData",
+  function ($http, $scope, urlService, manageData) {
+    var self = this;
+    
+  }]
+ });
+}(window.angular));

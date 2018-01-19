116e18cea3f775de32a0df7bda102e36fbf631bb
hy5man <smstoraj@gmail.com>
2018-01-19 12:27:47 +0530

    Payment Type

--- POS/app/payment_type/paymenttype.component.js
+++ POS/app/payment_type/paymenttype.component.js
@@ -7,7 +7,29 @@
            "controller"  : ["$http", "$scope", "urlService", "manageData",
   function ($http, $scope, urlService, manageData) {
     var self = this;
+    self.paymentTypeInput = [];
     
+    self.paymenttypes = [
+        {
+            "key": "cash",
+            "value": "Cash"
+        },
+        {
+            "key": "card",
+            "value": "Card"
+        }
+    ];
+    self.paymenttype_value = "cash";
+
+    self.add_payment_type = function() {
+      self.paymentTypeInput.push({ type_name: self.paymenttype_value, type_value: 0 });
+      //self.isEmptyMarket = false;
+    }
+
+    self.remove_payment_type = function(index) {
+      self.paymentTypeInput.splice(index);
+    }
+
   }]
  });
 }(window.angular));

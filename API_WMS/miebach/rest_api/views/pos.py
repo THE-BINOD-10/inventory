c1105bf078bd5ee1d347b5d52bc63e17c64c2364
sree522 <sreekanth.t1994@gmail.com>
2018-01-22 11:52:06 +0530

    Adding code for DC while delivering the pre order

--- API_WMS/miebach/rest_api/views/pos.py
+++ API_WMS/miebach/rest_api/views/pos.py
@@ -380,28 +380,28 @@ def customer_order(request):
     if only_return: order_ids=['return']
     return HttpResponse(json.dumps({'order_ids': order_ids}))
 
-
-@login_required
-@csrf_exempt
-def print_order_data(request):
+def prepare_delivery_challan_json(request, order_id, user_id):
+    json_data = {}
     customer_data, summary, gst_based = {}, {}, {}
     sku_data = []
     total_quantity, total_amount, subtotal = [0]*3
     status = 'fail'
     order_date = NOW
-    user_id = request.GET['user']
     user = User.objects.get(id = user_id)
-    order_id = request.GET['order_id']
     order_date = get_local_date(user, NOW)
     order_detail = OrderDetail.objects.filter(order_id = order_id,\
                                        user = user_id, quantity__gt = 0)
+
     for order in order_detail:
         selling_price = order.unit_price if order.unit_price != 0\
                                          else float(order.invoice_amount)/float(order.quantity)
-        tax_master = {'cgst_tax':0, 'sgst_tax':0, 'igst_tax':0, 'utgst_tax':0} if not order.sku else TaxMaster.objects\
-                                                            .filter(user=user_id, product_type=order.sku.product_type,\
-                                                            max_amt__gte=order.sku.price, min_amt__lte=order.sku.price)\
-                                                            .values('sgst_tax', 'cgst_tax', 'igst_tax', 'utgst_tax')[0]
+        tax_master_dict = TaxMaster.objects.filter(user=user_id, product_type=order.sku.product_type,
+                                                    max_amt__gte=order.sku.price,
+                                                   min_amt__lte=order.sku.price).\
+            values('sgst_tax', 'cgst_tax', 'igst_tax', 'utgst_tax')
+        tax_master = {'cgst_tax':0, 'sgst_tax':0, 'igst_tax':0, 'utgst_tax':0}
+        if tax_master_dict:
+            tax_master = tax_master_dict[0]
         gst_based.setdefault(tax_master['cgst_tax'], {'taxable_amt': 0,
                                                       'cgst_percent': tax_master["cgst_tax"],
                                                       'sgst_percent': tax_master["sgst_tax"],
@@ -446,13 +446,18 @@ def print_order_data(request):
                        'subtotal': total_amount,
                        'gst_based': gst_based,
                        'issue_type': order_summary.issue_type}
-    return HttpResponse(json.dumps({'data':
-                                        {'customer_data': customer_data,
-                                         'summary': summary,
-                                         'sku_data': sku_data,
-                                         'order_id': order_id,
-                                         'order_date': order_date},
-                                    'status': status}))
+            json_data = {'data':{'customer_data': customer_data, 'summary': summary,
+                                 'sku_data': sku_data, 'order_id': order_id,
+                                 'order_date': order_date}, 'status': status}
+    return json_data
+
+@login_required
+@csrf_exempt
+def print_order_data(request):
+    user_id = request.GET['user']
+    order_id = request.GET['order_id']
+    json_data = prepare_delivery_challan_json(request, order_id, user_id)
+    return HttpResponse(json.dumps(json_data))
 
 
 def get_order_details(order_id, user_id, mobile, customer_name, request_from):
@@ -538,11 +543,12 @@ def update_order_status(request):
       if data['delete_order'] == "true":
         order_detail.delete()
         if nw_status=="online":
-            return HttpResponse("Deleted Successfully !")
+            return HttpResponse(json.dumps({"message": "Deleted Successfully !", "data": {}}))
         else:
             continue
       for order in order_detail:
         order.status = 0
+        order.payment_received = order.invoice_amount
         sku = order.sku
         user_id = order.user
         user = User.objects.get(id = user_id)
@@ -550,7 +556,7 @@ def update_order_status(request):
         stock_detail = StockDetail.objects.filter(sku = sku, quantity__gt = 0)
         if stock_detail: order.save()
         else:
-           if nw_status=="online": return HttpResponse("Error")
+           if nw_status=="online": return HttpResponse(json.dumps({"message": "Error", "data": {}}))
            order.save()
            continue
 
@@ -588,7 +594,8 @@ def update_order_status(request):
         stock_quantity = stock_detail.aggregate(Sum('quantity'))['quantity__sum']
         picklist_creation(request, stock_detail, stock_quantity, order,\
                           picklist_number, stock_diff, item, user, invoice_number)
-  return HttpResponse("Delivered Successfully !")
+  json_data = prepare_delivery_challan_json(request, full_data[0]['order_id'], request.user.id)
+  return HttpResponse(json.dumps({"message": "Delivered Successfully !", "data": json_data}))
 
 
 @login_required

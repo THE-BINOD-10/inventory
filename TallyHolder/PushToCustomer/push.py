0decc95c9a38af880466dabdfe620f08c69c1d89
hy5man <smstoraj@gmail.com>
2018-03-21 18:54:12 +0530

    Scheduled Tasks in Windows

--- TallyHolder/PushToCustomer/push.py
+++ TallyHolder/PushToCustomer/push.py
@@ -1,7 +1,7 @@
-activate_this = 'C:\\Users\\Headrun\\Downloads\\TallyHolder\\TallyHolder\\headrunvenv\\Scripts\\activate_this.py'
-execfile(activate_this, dict(__file__=activate_this))
 import os, sys
-sys.path.append('C:\\Users\\Headrun\\Downloads\\TallyHolder\\TallyHolder\\TallyHolder')
+activate_this = os.path.abspath('../stockone/Scripts/activate_this.py')
+execfile(activate_this, dict(__file__=activate_this))
+sys.path.append('C:\\Users\\stockone\\Downloads\\Project\\WMS_ANGULAR\\TallyHolder')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TallyHolder.settings")
 import django
 django.setup()
@@ -13,7 +13,7 @@ from tally.tally.common_exceptions import *
 from PullFromStockone.models import *
 from tally.tally.logger_file import *
 
-bridge = TallyBridgeApp(dll="C:\\Users\\Headrun\\Downloads\\TallyHolder\\TallyHolder\\TallyHolder\\tally\\DLL\\TallyBridgeDll.dll")
+bridge = TallyBridgeApp(dll="C:\\Users\\stockone\\Downloads\\Project\\WMS_ANGULAR\\TallyHolder\\tally\\DLL\\TallyBridgeDll.dll")
 
 log = init_logger('logs/db_to_tally.log')
 
@@ -25,21 +25,23 @@ def push_item_master():
     resp_data = ItemMaster.objects.filter(push_status__in=[0,9])
     for obj in resp_data:
         try:
-            print obj.data
             d = json.loads(obj.data)
             status = bridge.item_master(d)
             obj.push_status = 1
             obj.save()
+	    log.info('Data Inserted to Tally : ' + str(obj))
         except TallyDataTransferError:
-            print traceback.format_exc()
-            if 'Duplicate Entry!' in TallyDataTransferError['message'] or 'already exists' in TallyDataTransferError['message']:
+	    log.debug(traceback.format_exc())
+	    if 'Duplicate Entry!' in TallyDataTransferError.message or 'already exists' in TallyDataTransferError.message:
                 obj.push_status = 1
                 obj.save()
+		log.info('Duplicate Entry : ' + str(obj))
             pass
         except:
             obj.push_status = 9
             obj.save()
-            print traceback.format_exc()
+	    log.info('Error Occured : ' + str(obj))
+	    log.debug(traceback.format_exc())
     log.info('------Item Master Completed Data transfer to Tally-----')
     return 0
 
@@ -49,15 +51,22 @@ def push_customer_vendor_master():
     for obj in resp_data:
         try:
             d = json.loads(obj.data)
-            print(d)
             status = bridge.customer_and_vendor_master(d)
             obj.push_status=1
             obj.save()
+	    log.info('Data Inserted to Tally : ' + str(obj))
         except TallyDataTransferError:
-            print traceback.format_exc()
+	    log.debug(traceback.format_exc())
+	    if 'Duplicate Entry!' in TallyDataTransferError.message or 'already exists' in TallyDataTransferError.message:
+		obj.push_status = 1
+		obj.save()
+		log.info('Duplicate Entry : ' + str(obj))
             pass
         except:
-            print traceback.format_exc()
+	    obj.push_status = 9
+	    obj.save()
+	    log.info('Error Occured : ' + str(obj))
+	    log.debug(traceback.format_exc())
             return traceback.format_exc()
     log.info('------Customer Master Completed Data transfer to Tally-----')
     return 0
@@ -68,18 +77,25 @@ def push_sales_invoice_data():
     for obj in resp_data:
         try:
             d = json.loads(obj.data)
-            print(d)
             for i in d['items']:
                 i['actual_qty'] = int(i['actual_qty'])
                 i['billed_qty'] = int(i['billed_qty'])
             status = bridge.sales_invoice(d)
             obj.push_status=1
             obj.save()
+	    log.info('Data Inserted to Tally : ' + str(obj))
         except TallyDataTransferError:
-            print traceback.format_exc()
+	    log.debug(traceback.format_exc())
+	    if 'Duplicate Entry!' in TallyDataTransferError.message or 'already exists' in TallyDataTransferError.message:
+		obj.push_status = 1
+		obj.save()
+		log.info('Duplicate Entry : ' + str(obj))
             pass
         except:
-            print traceback.format_exc()
+	    obj.push_status = 9
+	    obj.save()
+	    log.info('Error Occured : ' + str(obj))
+	    log.debug(traceback.format_exc())
             return traceback.format_exc()
     log.info('------Sales Invoice Completed Data transfer to Tally-----')
     return 0
@@ -113,7 +129,3 @@ def push_purchase_return_data():
         except:
             print traceback.format_exc()
             return traceback.format_exc()
-
-push_item_master()
-push_customer_vendor_master()
-push_sales_invoice_data()

de7dcc9d3528606ca10a77d935c0d41fdfd71c1d
VasuKuncham <vasu@headrun.com>
2018-04-19 19:10:49 +0530

    update DC data and other btns functionality

--- ANGULAR_WMS/app/scripts/controllers/outbound/customer_invoices/delivery_challan.js
+++ ANGULAR_WMS/app/scripts/controllers/outbound/customer_invoices/delivery_challan.js
@@ -91,7 +91,8 @@ function ServerSideProcessingCtrl($scope, $http, $state, $compile, $timeout, Ses
       vm.service.apiCall("generate_customer_invoice/", "GET", send).then(function(data){
         
         if (data.message) {
-          var mod_data = data.data;
+          vm.mod_data = {};
+          angular.copy(data.data, vm.mod_data);
           var modalInstance = $modal.open({
           templateUrl: 'views/outbound/customer_invoices/edit_delivery_challan.html',
           controller: 'EditDeliveryChallan',
@@ -101,7 +102,7 @@ function ServerSideProcessingCtrl($scope, $http, $state, $compile, $timeout, Ses
           keyboard: false,
           resolve: {
             items: function () {
-              return mod_data;
+              return vm.mod_data;
             }
           }
           });
@@ -112,6 +113,140 @@ function ServerSideProcessingCtrl($scope, $http, $state, $compile, $timeout, Ses
         }
       });
     }
+
+    vm.move_to = function (click_type) {
+      var po_number = '';
+      var status = false;
+      var field_name = "";
+      var data = [];
+      if (vm.user_type == 'distributor') {
+        data = vm.checked_ids;
+      } else {
+        angular.forEach(vm.selected, function(value, key) {
+          if(value) {
+            var temp = vm.dtInstance.DataTable.context[0].aoData[parseInt(key)]['_aData'];
+            if(!(po_number)) {
+              po_number = temp[temp['check_field']];
+            } else if (po_number != temp[temp['check_field']]) {
+              status = true;
+            }
+            field_name = temp['check_field'];
+            data.push(temp['id']);
+          }
+        });
+      }
+
+      if(status) {
+        vm.service.showNoty("Please select same "+field_name+"'s");
+      } else {
+
+        var ids = data.join(",");
+        var url = click_type === 'cancel_dc' ? 'move_to_dc/' : 'move_to_inv/';
+        var send = {seller_summary_id: ids};
+        if (click_type === 'cancel_dc') {
+          send['cancel'] = true;
+        }
+        vm.bt_disable = true;
+        vm.service.apiCall(url, "GET", send).then(function(data){
+          if(data.message) {
+            console.log(data.message);
+            vm.reloadData();
+          } else {
+            vm.service.showNoty("Something went wrong while moving to DC !!!");
+          }
+        })
+      }
+    };
+
+    vm.reloadData = function () {
+      $('.custom-table').DataTable().draw();
+    };
+
+    vm.generate_invoice = function(click_type, DC=false){
+
+      var po_number = '';
+      var status = false;
+      var field_name = "";
+      var data = [];
+      if (vm.user_type == 'distributor') {
+        data = vm.checked_ids;
+      } else {
+        angular.forEach(vm.selected, function(value, key) {
+          if(value) {
+            var temp = vm.dtInstance.DataTable.context[0].aoData[parseInt(key)]['_aData'];
+            if(!(po_number)) {
+              po_number = temp[temp['check_field']];
+            } else if (po_number != temp[temp['check_field']]) {
+              status = true;
+            }
+            field_name = temp['check_field'];
+            data.push(temp['id']);
+          }
+        });
+      }
+
+      if(status) {
+        vm.service.showNoty("Please select same "+field_name+"'s");
+      } else {
+
+        var ids = data.join(",");
+        var send = {seller_summary_id: ids};
+        if(po_number && field_name == 'SOR ID') {
+          send['sor_id'] = po_number;
+        }
+        if(click_type == 'edit'){
+          send['data'] = true;
+          send['edit_invoice'] = true;
+        }
+        send['delivery_challan'] = DC;
+        vm.delivery_challan = DC;
+        vm.bt_disable = true;
+        vm.service.apiCall("generate_customer_invoice/", "GET", send).then(function(data){
+
+          if(data.message) {
+            if(click_type == 'generate') {
+              vm.pdf_data = data.data;
+              if(typeof(vm.pdf_data) == "string" && vm.pdf_data.search("print-invoice") != -1) {
+                $state.go("app.outbound.CustomerInvoices.InvoiceE");
+                $timeout(function () {
+                  $(".modal-body:visible").html(vm.pdf_data)
+                }, 3000);
+              } else if(Session.user_profile.user_type == "marketplace_user") {
+                $state.go("app.outbound.CustomerInvoices.InvoiceM");
+              } else if(vm.permissions.detailed_invoice) {
+                $state.go("app.outbound.CustomerInvoices.InvoiceD");
+              } else {
+                $state.go("app.outbound.CustomerInvoices.InvoiceN");
+              }
+            } else {
+              var mod_data = data.data;
+              var modalInstance = $modal.open({
+              templateUrl: 'views/outbound/toggle/edit_invoice.html',
+              controller: 'EditInvoice',
+              controllerAs: 'pop',
+              size: 'lg',
+              backdrop: 'static',
+              keyboard: false,
+              resolve: {
+                items: function () {
+                  return mod_data;
+                }
+              }
+              });
+
+              modalInstance.result.then(function (selectedItem) {
+                var data = selectedItem;
+              })
+            }
+          }
+          vm.bt_disable = false;
+        });
+      }
+    }
+
+    vm.close = function() {
+      $state.go("app.outbound.CustomerInvoices")
+    };
 }
 
 function EditDeliveryChallan($scope, $http, $state, $timeout, Session, colFilters, Service, $stateParams, $modalInstance, items) {
@@ -126,6 +261,89 @@ function EditDeliveryChallan($scope, $http, $state, $timeout, Session, colFilter
   vm.ok = function () {
     $modalInstance.close("close");
   };
+
+  vm.update_data = function (index) {
+    console.log(index);
+    if (index == vm.model_data.data.length-1) {
+      vm.model_data.data.push({"sku_code": "", "sku_class": "", "pkng": "", "quantity": ""});
+    } else {
+      if(vm.model_data.data[index].order_id){
+        vm.delete_data('order_id', vm.model_data.data[index].order_id, index);
+      } else {
+        vm.delete_data('id', vm.model_data.data[index].id, index);
+      }
+      vm.model_data.data.splice(index,1);
+      vm.getTotals();
+    }
+  };
+
+  vm.delete_data = function(key, id, index) {
+    if(id) {
+      var del_data = {}
+      del_data[key] = id;
+      vm.service.apiCall('delete_po/', 'GET', del_data).then(function(data){
+        if(data.message) {
+    vm.model_data.data[index].row_price = (vm.model_data.data[index].order_quantity * Number(vm.model_data.data[index].price))
+;
+    vm.model_data.total_price = 0;
+
+    angular.forEach(vm.model_data.data, function(one_row){
+      vm.model_data.total_price = vm.model_data.total_price + (one_row.order_quantity * one_row.price);
+    });
+          vm.service.pop_msg(data.data);
+        }
+      });
+    }
+  };
+
+  vm.getTotals = function(data) {
+    vm.model_data.total_price = 0;
+    vm.model_data.sub_total = 0;
+    angular.forEach(vm.model_data.data, function(sku_data){
+      var temp = sku_data.order_quantity * sku_data.price;
+      if (!sku_data.tax) {
+        sku_data.tax = Number(sku_data.cgst_tax) + Number(sku_data.sgst_tax) + Number(sku_data.igst_tax) + Number(sku_data.utgst_tax);
+      }
+      vm.model_data.total_price = vm.model_data.total_price + temp;
+      vm.model_data.sub_total = vm.model_data.sub_total + ((temp / 100) * sku_data.tax) + temp;
+    })
+  }
+
+  vm.get_sku_details = function(product, item, index) {
+    product.wms_code = item.wms_code;
+    product.measurement_unit = item.measurement_unit;
+    product.description = item.sku_desc;
+    product.order_quantity = 1;
+    product.price = "";
+    product.description = item.sku_desc;
+    product.sgst_tax = "";
+    product.cgst_tax = "";
+    product.igst_tax = "";
+    product.utgst_tax = "";
+    product.tax = "";
+    vm.getTotals();
+
+    if(vm.model_data.receipt_type == 'Hosted Warehouse') {
+
+      vm.model_data.supplier_id = vm.model_data.seller_supplier_map[vm.model_data.seller_type.split(":")[0]];
+    }
+
+    if (!vm.model_data.supplier_id){
+      return false;
+    } else {
+      var supplier = vm.model_data.supplier_id;
+      $http.get(Session.url+'get_mapping_values/?wms_code='+product.wms_code+'&supplier_id='+supplier, {withCredentials : true}).success(function(data, status, headers, config) {
+        if(Object.values(data).length) {
+          product.price = data.price;
+          product.supplier_code = data.supplier_code;
+          product.ean_number = data.ean_number;
+
+          vm.model_data.data[index].row_price = (vm.model_data.data[index].order_quantity * Number(vm.model_data.data[index].price));
+          vm.getTotals();
+        }
+      });
+    }
+  }
 
 }
 

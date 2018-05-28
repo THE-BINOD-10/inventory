e0add3c569c3f9ef8cf984ff2caaedaec5d82608
akhilrajvc <akhil@mieone.com>
2018-05-28 14:55:52 +0530

    new UI files

---
+++ ANGULAR_WMS/app/scripts/controllers/inbound/supplier_invoices/po_challan.js
@@ -0,0 +1,85 @@
+'use strict';
+
+angular.module('urbanApp', ['datatables'])
+  .controller('POChallanCtrl',['$scope', '$http', '$state', '$compile', '$timeout', 'Session','DTOptionsBuilder', 'DTColumnBuilder', 'DTColumnDefBuilder', 'colFilters', 'Service', '$modal', ServerSideProcessingCtrl]);
+
+function ServerSideProcessingCtrl($scope, $http, $state, $compile, $timeout, Session, DTOptionsBuilder, DTColumnBuilder, DTColumnDefBuilder, colFilters, Service, $modal) {
+
+    var vm = this;
+    vm.apply_filters = colFilters;
+
+
+    vm.service = Service;
+    vm.permissions = Session.roles.permissions;
+    vm.user_type = Session.roles.permissions.user_type;
+
+    vm.selected = {};
+    vm.checked_items = {};
+    vm.selectAll = false;
+    vm.bt_disable = true;
+    vm.display = false;
+
+    var send = {'tabType': 'POChallans'};
+    vm.service.apiCall("supplier_invoice_data/", "GET", send).then(function(data) {
+      if(data.message) {
+        vm.filters = {'datatable': 'POChallans', 'search0':'', 'search1':'', 'search2': '', 'search3': ''}
+        vm.dtOptions = DTOptionsBuilder.newOptions()
+          .withOption('ajax', {
+              url: Session.url+'results_data/',
+              type: 'POST',
+              data: vm.filters,
+              xhrFields: {
+                withCredentials: true
+              }
+          })
+          .withDataProp('data')
+          .withOption('drawCallback', function(settings) {
+            vm.service.make_selected(settings, vm.selected);
+          })
+          .withOption('processing', true)
+          .withOption('serverSide', true)
+          .withOption('order', [5, 'desc'])
+          .withOption('createdRow', function(row, data, dataIndex) {
+             $compile(angular.element(row).contents())($scope);
+          })
+          .withOption('headerCallback', function(header) {
+            if (!vm.headerCompiled) {
+                vm.headerCompiled = true;
+                $compile(angular.element(header).contents())($scope);
+            }
+          })
+          .withPaginationType('full_numbers')
+          .withOption('initComplete', function( settings ) {
+            //vm.apply_filters.add_search_boxes("#"+vm.dtInstance.id);
+          });
+
+        var columns = data.data.headers;
+        var not_sort = ['Order Quantity', 'Picked Quantity']
+        vm.dtColumns = vm.service.build_colums(columns, not_sort);
+        vm.dtColumns.unshift(DTColumnBuilder.newColumn(null).withTitle('').notSortable().withOption('width', '20px')
+               .renderWith(function(data, type, full, meta) {
+                 if( 1 == vm.dtInstance.DataTable.context[0].aoData.length) {
+                   vm.selected = {};
+                 }
+                 vm.selected[meta.row] = vm.selectAll;
+                 return vm.service.frontHtml + meta.row + vm.service.endHtml;
+               }))
+
+        vm.dtInstance = {};
+
+        $scope.$on('change_filters_data', function(){
+          if($("#"+vm.dtInstance.id+":visible").length != 0) {
+            vm.dtInstance.DataTable.context[0].ajax.data[colFilters.label] = colFilters.value;
+            vm.service.refresh(vm.dtInstance);
+          }
+        });
+        vm.display = true;
+      }
+    });
+
+
+
+
+
+
+}

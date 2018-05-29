c6bf97b83fa9fb4ef648251185168dac7221e191
Vinay <vinay@notemonk.com>
2018-05-29 16:02:35 +0530

    uncheck checkboxes after move to next tab

--- ANGULAR_WMS/app/scripts/controllers/inbound/supplier_invoices_main.js
+++ ANGULAR_WMS/app/scripts/controllers/inbound/supplier_invoices_main.js
@@ -53,16 +53,15 @@ function ServerSideProcessingCtrl($scope, $http, $state, $compile, $timeout, Ses
             .withOption('initComplete', function( settings ) {
               //vm.apply_filters.add_search_boxes("#"+vm.dtInstance.id);
             });
-
           var columns = data.data.headers;
           var not_sort = ['Order Quantity', 'Picked Quantity']
           vm.dtColumns = vm.service.build_colums(columns, not_sort);
           var row_click_bind = 'td';
           vm.dtColumns.unshift(DTColumnBuilder.newColumn(null).withTitle('').notSortable().withOption('width', '20px')
                  .renderWith(function(data, type, full, meta) {
-                   // if( 1 == vm.dtInstance.DataTable.context[0].aoData.length) {
-                   //   vm.selected = {};
-                   // }
+                   if( 1 == vm.dtInstance.DataTable.context[0].aoData.length) {
+                     vm.selected = {};
+                   }
                    // vm.selected[meta.row] = vm.selectAll;
                    // return "<i style='cursor: pointer' ng-click='showCase.addRowData($event, "+JSON.stringify(full)+")' class='fa fa-plus-square'></i>";
                    return vm.service.frontHtml + meta.row + vm.service.endHtml;
@@ -80,129 +79,72 @@ function ServerSideProcessingCtrl($scope, $http, $state, $compile, $timeout, Ses
         }
       });
 
-		/*var send = {'tabType': 'ProcessedPOs'};
-		vm.service.apiCall("supplier_invoice_data/", "GET", send).then(function(data) {
-
-        if(data.message) {
-          vm.filters = {'datatable': 'ProcessedPOs', 'search0':'', 'search1':'', 'search2': '', 'search3': '', 'search4': '', 'search5': ''}
-          vm.dtOptions = DTOptionsBuilder.newOptions()
-            .withOption('ajax', {
-                url: Session.url+'results_data/',
-                type: 'POST',
-                data: vm.filters,
-                xhrFields: {
-                  withCredentials: true
-                }
-            })
-            .withDataProp('data')
-            .withOption('drawCallback', function(settings) {
-              vm.service.make_selected(settings, vm.selected);
-            })
-            .withOption('processing', true)
-            .withOption('serverSide', true)
-            .withOption('order', [5, 'desc'])
-            .withOption('createdRow', function(row, data, dataIndex) {
-               $compile(angular.element(row).contents())($scope);
-            })
-            .withOption('headerCallback', function(header) {
-              if (!vm.headerCompiled) {
-                  vm.headerCompiled = true;
-                  $compile(angular.element(header).contents())($scope);
-              }
-            })
-            .withPaginationType('full_numbers')
-            .withOption('initComplete', function( settings ) {
-              //vm.apply_filters.add_search_boxes("#"+vm.dtInstance.id);
-            });
-
-          var columns = data.data.headers;
-          var not_sort = ['Order Quantity', 'Picked Quantity']
-          vm.dtColumns = vm.service.build_colums(columns, not_sort);
-          vm.dtColumns.unshift(DTColumnBuilder.newColumn(null).withTitle('').notSortable().withOption('width', '20px')
-                 .renderWith(function(data, type, full, meta) {
-                   if( 1 == vm.dtInstance.DataTable.context[0].aoData.length) {
-                     vm.selected = {};
-                   }
-                   vm.selected[meta.row] = vm.selectAll;
-                   return vm.service.frontHtml + meta.row + vm.service.endHtml;
-                 }))
-
-          vm.dtInstance = {};
-
-          $scope.$on('change_filters_data', function(){
-            if($("#"+vm.dtInstance.id+":visible").length != 0) {
-              vm.dtInstance.DataTable.context[0].ajax.data[colFilters.label] = colFilters.value;
-              vm.service.refresh(vm.dtInstance);
-            }
-          });
-          vm.display = true;
-        }
-      });*/
-
-
-
-
 	}//end if market_place_user
 
   vm.checked_ids = [];
-    vm.checkedItem = function(data, index){
+  vm.checkedItem = function(data, index){
+    
+    if (vm.checked_items[index]) {
       
-      if (vm.checked_items[index]) {
-        
-        delete vm.checked_items[index];
-      } else {
-        
-        vm.checked_items[index] = data;
-        vm.checked_ids.push(vm.checked_items[index].id);
-      }
-      // vm.checked_items[index] = data;
-      console.log(data)      
+      delete vm.checked_items[index];
+    } else {
+      
+      vm.checked_items[index] = data;
+      vm.checked_ids.push(vm.checked_items[index].id);
+    }
+    // vm.checked_items[index] = data;
+    console.log(data)      
+  }
+
+  vm.reloadData = function () {
+    $('.custom-table').DataTable().draw();
+  };
+
+
+  vm.move_to = function (click_type) {
+    var supplier_name = '';
+    var status = false;
+    var field_name = "";
+    var data = [];
+    if (vm.user_type == 'distributor') {
+      data = vm.checked_ids;
+    } else {
+      angular.forEach(vm.selected, function(value, key) {
+        if(value) {
+          var temp = vm.dtInstance.DataTable.context[0].aoData[parseInt(key)]['_aData'];
+          if(!(supplier_name)) {
+            supplier_name = temp['Supplier Name'];
+          } else if (supplier_name != temp['Supplier Name']) {
+            status = true;
+          }
+          field_name = temp['check_field'];
+          var grn_no = temp['GRN No'];
+          grn_no = grn_no.split('/');
+
+          var send_data = JSON.stringify({grn_no: grn_no, seller_summary_name: supplier_name, seller_summary_id: temp['id']});
+
+          data.push(send_data);
+        }
+      });
     }
 
-    vm.move_to = function (click_type) {
-      var supplier_name = '';
-      var status = false;
-      var field_name = "";
-      var data = [];
-      if (vm.user_type == 'distributor') {
-        data = vm.checked_ids;
-      } else {
-        angular.forEach(vm.selected, function(value, key) {
-          if(value) {
-            var temp = vm.dtInstance.DataTable.context[0].aoData[parseInt(key)]['_aData'];
-            if(!(supplier_name)) {
-              supplier_name = temp['Supplier Name'];
-            } else if (supplier_name != temp['Supplier Name']) {
-              status = true;
-            }
-            field_name = temp['check_field'];
-            var grn_no = temp['GRN No'];
-            grn_no = grn_no.split('/');
+    if(status) {
+      vm.service.showNoty("Please select same "+field_name+"'s");
+    } else {
 
-            var send_data = JSON.stringify({grn_no: grn_no, seller_summary_name: supplier_name, seller_summary_id: temp['id']});
-
-            data.push(send_data);
-          }
-        });
-      }
-
-      if(status) {
-        vm.service.showNoty("Please select same "+field_name+"'s");
-      } else {
-
-        var send = data.join(",");
-        send = {data: send}
-        var url = click_type === 'move_to_po_challan' ? 'move_to_po_challan/' : 'move_to_inv/';
-        vm.bt_disable = true;
-        vm.service.apiCall(url, "GET", send).then(function(data){
-          if(data.message) {
-            console.log(data.message);
-            vm.reloadData();
-          } else {
-            vm.service.showNoty("Something went wrong while moving to po challan !!!");
-          }
-        })
-      }
-    };
+      var send = data.join(",");
+      send = {data: send}
+      var url = click_type === 'move_to_po_challan' ? 'move_to_po_challan/' : 'move_to_inv/';
+      vm.bt_disable = true;
+      vm.service.apiCall(url, "GET", send).then(function(data){
+        if(data.message) {
+          console.log(data.message);
+          vm.reloadData();
+        } else {
+          vm.service.showNoty("Something went wrong while moving to po challan !!!");
+        }
+      })
+    }
+  };
 
 }

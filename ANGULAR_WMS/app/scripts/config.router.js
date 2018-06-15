d4c1646db14e32f2925718903cf883ef3d1210f9
VasuKuncham <vasu@headrun.com>
2018-06-15 15:19:17 +0530

    created new style page layout

--- ANGULAR_WMS/app/scripts/config.router.js
+++ ANGULAR_WMS/app/scripts/config.router.js
@@ -2121,6 +2121,15 @@ var app = angular.module('urbanApp')
               }]
             }
           })
+          .state('user.App.newStyle', {
+            url: '/newStyle',
+            templateUrl: 'views/outbound/app/create_orders/newstyle.html',
+            resolve: {
+              deps: ['$ocLazyLoad', function ($ocLazyLoad) {
+                return $ocLazyLoad.load('scripts/controllers/outbound/app/newstyle.js');
+              }]
+            }
+          })
           .state('user.App.Cart', {
             url: '/Cart',
             templateUrl: 'views/outbound/app/create_orders/order.html',

f4d86f74dec14009841c1530a5d620574b6b9cb2
Vinay <vinay@notemonk.com>
2018-07-10 19:11:12 +0530

    Added menus in A and T module

--- ANGULAR_WMS/app/scripts/controllers/outbound/app/newstyle.js
+++ ANGULAR_WMS/app/scripts/controllers/outbound/app/newstyle.js
@@ -8,6 +8,12 @@ function AppNewStyle ($scope, $http, $q, Session, colFilters, Service, $state, $
   var vm = this;
   vm.place_order_loading = false;
 
+  vm.main_menus = [{'name':'House Keeping', 'sub_menus':['Menu-1','Menu-2','Menu-3','Menu-4']},
+  				   {'name':'Office Stationary', 'sub_menus':['Menu-1','Menu-2','Menu-3','Menu-4']},
+  				   {'name':'Content Management', 'sub_menus':['Menu-1','Menu-2','Menu-3','Menu-4']},
+  				   {'name':'Office Technologies', 'sub_menus':['Menu-1','Menu-2','Menu-3','Menu-4']},
+  				   {'name':'Promotional Items', 'sub_menus':['Menu-1','Menu-2','Menu-3','Menu-4']},
+  				   {'name':'Display Systems', 'sub_menus':['Menu-1','Menu-2','Menu-3','Menu-4']}];
 
 } // End of funciton
 

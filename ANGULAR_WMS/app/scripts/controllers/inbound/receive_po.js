4e913fe96ea88a71e1e93b7f144ddeee7a8a6c97
Vinay <vinay@notemonk.com>
2018-07-25 16:21:09 +0530

    condition added to scope apply

--- ANGULAR_WMS/app/scripts/controllers/inbound/receive_po.js
+++ ANGULAR_WMS/app/scripts/controllers/inbound/receive_po.js
@@ -272,14 +272,23 @@ function ServerSideProcessingCtrl($scope, $http, $state, $timeout, Session, DTOp
     vm.add_wms_code = add_wms_code;
     function add_wms_code(index=0, flag=true) {
       if (index==vm.model_data.data.length-1 && !flag || !index && flag) {
-        $scope.$apply(function() {
+        if (flag) {
+
           vm.model_data.data.push([{"wms_code":"", "po_quantity":0, "receive_quantity":"", "price":"", "dis": false,
                                   "order_id": '', "is_new": true, 'mrp': 0, "unit": "",
                                   "buy_price": "", "cess_percent": "", "tax_percent": "", "total_amt": "",
                                   "discount_percentage": 0,
                                   "sku_details": [{"fields": {"load_unit_handle": ""}}]}]);
-        });
-        //vm.new_sku = true
+        } else {
+
+          $scope.$apply(function() {
+            vm.model_data.data.push([{"wms_code":"", "po_quantity":0, "receive_quantity":"", "price":"", "dis": false,
+                                    "order_id": '', "is_new": true, 'mrp': 0, "unit": "",
+                                    "buy_price": "", "cess_percent": "", "tax_percent": "", "total_amt": "",
+                                    "discount_percentage": 0,
+                                    "sku_details": [{"fields": {"load_unit_handle": ""}}]}]);
+          });
+        }
       }
     }
     vm.get_sku_details = function(data, selected) {

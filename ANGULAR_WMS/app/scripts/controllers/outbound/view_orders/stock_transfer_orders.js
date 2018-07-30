45f16e613de8278489dc9066e6f75b43b98b348d
Vinay <vinay@notemonk.com>
2018-07-30 15:08:49 +0530

    scope apply stoped

--- ANGULAR_WMS/app/scripts/controllers/outbound/view_orders/stock_transfer_orders.js
+++ ANGULAR_WMS/app/scripts/controllers/outbound/view_orders/stock_transfer_orders.js
@@ -276,12 +276,12 @@ function ServerSideProcessingCtrl($scope, $http, $state, $compile, $timeout, Ses
           vm.model_data['data'] = [];
         }
 
-        $scope.$apply(function() {
+        // $scope.$apply(function() {
 
           vm.model_data.data.push({item_code:'', product_title:'', quantity:0, unit_price:0, invoice_amount:0, 
             opening_stock: '', received: '', total_stock: '', consumed: '', closing: '', new_product:true, 
             default_status: false, sku_status: 1});
-        });
+        // });
       } else {
         var data_to_delete = {};
         data_to_delete['order_id'] = vm.order_id;

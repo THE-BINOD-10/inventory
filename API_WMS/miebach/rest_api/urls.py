0b110fdb89f02a8a9ac768eac2bc1b83792d83db
saipavan0210 <sai@headrun.com>
2018-09-07 19:41:20 +0530

    1. Listing all reseller's corporates in Dropdown while creating custom order. 2. Reseller Can confirm the order or the hold the stock if MD Approves the order.

--- API_WMS/miebach/rest_api/urls.py
+++ API_WMS/miebach/rest_api/urls.py
@@ -311,6 +311,7 @@ urlpatterns = [
     url(r'^remove_manual_enquiry_image/$', remove_manual_enquiry_image),
     url(r'^save_manual_enquiry_image/$', save_manual_enquiry_image),
     url(r'^request_manual_enquiry_approval/$', request_manual_enquiry_approval),
+    url(r'^confirm_or_hold_custom_order/$', confirm_or_hold_custom_order),
     url(r'^get_invoice_details/$', get_invoice_details),
     url(r'^update_cust_profile/$', update_cust_profile),
     url(r'^print_cartons_data/$', print_cartons_data),

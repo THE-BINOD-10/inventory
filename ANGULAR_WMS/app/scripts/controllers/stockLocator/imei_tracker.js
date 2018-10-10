82b563aec957142ef4fddfb99b2815e816da272e
Vinay Kumar <vinay@headrun.com>
2018-10-10 14:01:07 +0530

    JO Details Displaying

--- ANGULAR_WMS/app/scripts/controllers/stockLocator/imei_tracker.js
+++ ANGULAR_WMS/app/scripts/controllers/stockLocator/imei_tracker.js
@@ -5,11 +5,12 @@ function IMEITrackerCtrl($scope, $http, Service) {
   var vm = this;
 
   vm.model_data = {};
+  vm.scan_item_type = '';
   vm.scan_imei = function(event, field) {
       if ( event.keyCode == 13 && field) {
 
         Service.apiCall('get_imei_data/', 'GET', {imei:field}, true).then(function(data){
-
+          vm.scan_item_type = 'JO';
           if(data.data.message == "Success") {
             vm.model_data = data.data;
           } else {

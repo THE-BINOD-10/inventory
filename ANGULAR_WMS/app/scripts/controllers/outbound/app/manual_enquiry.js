924670e8a194bdd1264612facbf33d983670eecd
Vinay Kumar <vinay@headrun.com>
2018-11-05 13:52:17 +0530

    Multiple images showing before upload

--- ANGULAR_WMS/app/scripts/controllers/outbound/app/manual_enquiry.js
+++ ANGULAR_WMS/app/scripts/controllers/outbound/app/manual_enquiry.js
@@ -153,18 +153,23 @@ function AppManualEnquiry($scope, $http, $q, Session, colFilters, Service, $stat
     }
   }
 
-  function readURL(input) {
-    if (input.files && input.files[0]) {
-      var reader = new FileReader();
-      reader.onload = function(e) {
-        $('#showImage').attr('src', e.target.result);
+  var imagesPreview = function(input, placeToInsertImagePreview) {
+    if (input.files) {
+        var filesAmount = input.files.length;
+        for (var i = 0; i < filesAmount; i++) {
+          var reader = new FileReader();
+          reader.onload = function(event) {
+          // $($.parseHTML('<div class="col-md-3" ng-mouseover="Enquiry.show_remove[$index]=true" ng-mouseleave="Enquiry.show_remove[$index]=false"><img src="'+event.target.result+'" width="100%"><button class="btn btn-danger mr10" ng-hide="Enquiry.show_remove[$index]"  ng-if="Enquiry.show_remove[$index]" style="position:absolute;top:0px;right:0px;" ng-click="Enquiry.remove_image($index)">X</button></div>')).appendTo(placeToInsertImagePreview);
+          $($.parseHTML('<div class="col-md-3 dyn_imgs no-padding m5"><img src="'+event.target.result+'" width="100%"></div>')).appendTo(placeToInsertImagePreview);
+        }
+        reader.readAsDataURL(input.files[i]);
       }
-      reader.readAsDataURL(input.files[0]);
     }
-  }
+  };
 
-  $("#image-upload").change(function() {
-    readURL(this);
+  $('#image-upload').on('change', function() {
+    $("div.multi_imgs_display").empty();
+    imagesPreview(this, 'div.multi_imgs_display');
   });
 
   $ctrl.upload_name = [];

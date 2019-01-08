96e02e5e77721aec0f75abce955fb62938da2f3d
root <avadhani@headrun.com>
2019-01-08 17:21:49 +0530

    adding dynamic trade solutions batch

--- API_WMS/miebach/rest_api/views/outbound.py
+++ API_WMS/miebach/rest_api/views/outbound.py
@@ -801,10 +801,10 @@ def get_picklist_data(data_id, user_id):
     picklist_orders = Picklist.objects.filter(Q(order__sku__user=user_id) | Q(stock__sku__user=user_id),
                                               picklist_number=data_id)
     pick_stocks = StockDetail.objects.filter(sku__user=user_id)
-    stocks = pick_stocks.filter(quantity__gt=0).values('sku__wms_code', 'location__location').distinct().annotate(
+    stocks = pick_stocks.filter(quantity__gt=0).values('sku__wms_code', 'location__location','batch_detail__batch_no').distinct().annotate(
         quantity=Sum('quantity'))
     reserved_instances = PicklistLocation.objects.filter(status=1, picklist__order__user=user_id).values(
-        'stock__sku__wms_code', 'stock__location__location'). \
+        'stock__sku__wms_code', 'stock__location__location','stock__batch_detail__batch_no'). \
         distinct().annotate(reserved=Sum('reserved'))
     stock_skus = map(lambda d: d['sku__wms_code'], stocks)
     reserved_skus = map(lambda d: d['stock__sku__wms_code'], reserved_instances)
@@ -843,6 +843,7 @@ def get_picklist_data(data_id, user_id):
             order_code = ''
             mrp = ''
             batch_no = ''
+            manufactured_date =''
             courier_name = ''
             if order.stock:
                 stock_id = pick_stocks.get(id=order.stock_id)
@@ -904,8 +905,9 @@ def get_picklist_data(data_id, user_id):
                 if stock_id.batch_detail:
                     mrp = stock_id.batch_detail.mrp
                     batch_no = stock_id.batch_detail.batch_no
+                    manufactured_date = datetime.datetime.strftime(stock_id.batch_detail.manufactured_date, "%d/%m/%Y")
 
-            match_condition = (location, pallet_detail, wms_code, sku_code, title)
+            match_condition = (location, batch_no, manufactured_date,pallet_detail, wms_code, sku_code, title)
             if match_condition not in batch_data:
                 if order.reserved_quantity == 0:
                     continue
@@ -931,6 +933,7 @@ def get_picklist_data(data_id, user_id):
                                                'stock_left': stock_left, 'last_picked_locs': last_picked_locs,
                                                'customer_name': customer_name, 'customer_address': customer_address,
                                                'marketplace': marketplace,
+                                               'manufactured_date':manufactured_date,
                                                'order_no': order_id, 'remarks': remarks,
                                                'load_unit_handle': load_unit_handle, 'category': category,
                                                'original_order_id': original_order_id, 'mrp':mrp,

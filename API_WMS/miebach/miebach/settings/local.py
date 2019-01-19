3fa14642d2add939a23b21b902c43a1cac8915ef
sandhani <aravind@mieone.com>
2019-01-19 17:52:17 +0530

    Local

--- API_WMS/miebach/miebach/settings/local.py
+++ API_WMS/miebach/miebach/settings/local.py
@@ -4,9 +4,9 @@ from .base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'aravind_prod_rista_dm',
+        'NAME': 'STAGING',
         'USER': 'root',
-        'PASSWORD': '0510^2017',
+        'PASSWORD': 'krothapalli',
         'TEST_MIRROR': 'default'
     }
 }
@@ -14,6 +14,6 @@ DATABASES = {
 ONESIGNAL_AUTH_KEY = 'YTc3ZGRiZjctZDZkZS00ZGMyLWFhZmMtOTUyNzdhMDJiOGUx'
 ONESIGNAL_APP_ID = '98737db9-a2c9-4ff7-be74-42149f21679f'  #SM_NEW_PUSH
 
-INTEGRATIONS_CFG_FILE = 'rest_api/views/configuration_staging.cfg'
+INTEGRATIONS_CFG_FILE = 'rest_api/views/configuration_local.cfg'
 
 SERVICE_WORKER_VERSION = '0.0.1-build03.0.99'

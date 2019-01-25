5cbcd8a88290d5929968ac7d5aab84ca93880474
mkaladhar <aravind@mieone.com>
2019-01-25 02:43:41 -0500

    Added Logs and SKU - Create and Update done

--- API_WMS/miebach/api_calls/views.py
+++ API_WMS/miebach/api_calls/views.py
@@ -20,6 +20,10 @@ from django.db.models import Q, F
 from django.core.serializers.json import DjangoJSONEncoder
 from rest_api.views.utils import *
 log = init_logger('logs/integrations.log')
+storehippo_log = init_logger('logs/storehippo.log')
+create_order_storehippo_log = init_logger('logs/create_order_storehippo_log.log')
+create_update_sku_storehippo_log = init_logger('logs/create_update_sku_storehippo_log.log')
+
 # Create your views here.
 
 NOW = datetime.datetime.now()
@@ -1421,7 +1425,7 @@ def store_hippo_line_items(line_items):
 
 
 def create_order_storehippo(store_hippo_data, user_obj):
-    import pdb;pdb.set_trace()
+    create_order_storehippo_log.info('Create Store Hippo Data' + str(store_hippo_data))
     allOrders = []
     create_order = {}
     #create_order['extra_key'] = []
@@ -1445,7 +1449,6 @@ def create_order_storehippo(store_hippo_data, user_obj):
     create_order['status'] = store_hippo_data['status']
     create_order['fulfillmentStatus'] = store_hippo_data['fulfillment_status']
     create_order['invoice_amount'] = create_order['all_total_items'] + create_order['all_total_tax'] + create_order['shipping_charges'] - store_hippo_data['discounts_total']
-    import pdb;pdb.set_trace()
     #create_order['extra_key'].append({'shipping_charges':store_hippo_data['shipping_total']})
     allOrders.append(create_order)
     try:
@@ -1459,34 +1462,88 @@ def create_order_storehippo(store_hippo_data, user_obj):
                 failed_status = failed_status[0]
                 failed_status.update({'Status': 'Failure'})
             return json.dumps(failed_status)
+        create_order_storehippo_log.info('StoreHippo Data Sent to Stockone ' + str(final_data_dict))
         status = update_order_dicts_storehippo(final_data_dict, user=user_obj, company_name='mieone')
-        log.info(status)
+        create_order_storehippo_log.info(status)
         return status
     except Exception as e:
         import traceback
-        log.debug(traceback.format_exc())
-        log.info('Update orders data failed for %s and params are %s and error statement is %s' % (str(user_obj.username), str(request.body), str(e)))
+        create_order_storehippo_log.debug(traceback.format_exc())
+        create_order_storehippo_log.info('Update orders data failed for %s and params are %s and error statement is %s' % (str(user_obj.username), str(request.body), str(e)))
         status = {'messages': 'Internal Server Error', 'status': 0}
         return status
 
 
-def create_sku_storehippo(store_hippo_data, user_obj):
-    print store_hippo_data
-    return user_obj
+def create_update_sku_storehippo(store_hippo_data, user_obj):
+    create_update_sku_storehippo_log.info('StoreHippo Data ' + str(store_hippo_data))
+    stockone_data = {}
+    stockone_data['sku_code'] = store_hippo_data.get('uniquesku', '')[0]
+    stockone_data['wms_code'] = store_hippo_data.get('uniquesku', '')[0]
+    stockone_data['sku_desc'] = store_hippo_data.get('description', '')
+    stockone_data['sku_group'] = ''
+    stockone_data['sku_type'] = ''
+    stockone_data['sku_category'] = store_hippo_data.get('categories', '')[0]
+    stockone_data['sku_class'] = ''
+    stockone_data['threshold_quantity'] = store_hippo_data.get('inventory_low_stock_quantity', 0)
+    stockone_data['online_percentage'] = 0
+    stockone_data['image_url'] = store_hippo_data.get('images','')[0]['tempSrc']
+    stockone_data['qc_check'] = 0
+    stockone_data['status'] = store_hippo_data.get('publish', 0)
+    #stockone_data['zone_id'] = 0
+    stockone_data['relation_type'] = ''
+    stockone_data['discount_percentage'] = 0
+    stockone_data['price'] = store_hippo_data.get('price', 0)
+    stockone_data['product_type'] = ''
+    stockone_data['sku_brand'] = store_hippo_data.get('brand', '')
+    stockone_data['sku_size'] = ''
+    stockone_data['style_name'] = ''
+    stockone_data['mrp'] = store_hippo_data.get('price', 0)
+    stockone_data['sequence'] = 0
+    stockone_data['measurement_type'] = ''
+    stockone_data['sale_through'] = ''
+    stockone_data['color'] = ''
+    #stockone_data['ean_number'] = store_hippo_data.get('_id', 0)
+    stockone_data['mix_sku'] = ''
+    stockone_data['load_unit_handle'] = ''
+    stockone_data['hsn_code'] = 0
+    stockone_data['sub_category'] = ''
+    stockone_data['primary_category'] = ''
+    stockone_data['cost_price'] = store_hippo_data.get('price', 0)
+    stockone_data['shelf_life'] = 0
+    stockone_data['enable_serial_based'] = 0
+    stockone_data['youtube_url'] = ''
+    stockone_data['user'] = user_obj.id
+    try:
+        sku_obj = SKUMaster.objects.filter(**{'user':user_obj.id, 'sku_code':store_hippo_data.get('uniquesku', '')[0]})
+        if not sku_obj:
+            sku_query_obj = SKUMaster.objects.create(**stockone_data)
+            create_update_sku_storehippo_log.info('Created SKU ' + str(stockone_data))
+        else:
+            sku_query_obj = sku_obj.update(**stockone_data)
+            create_update_sku_storehippo_log.info('Updated SKU '+ str(stockone_data))
+    except:
+        sku_query_obj = "Error Occured"
+        create_update_sku_storehippo_log.info('Error Occured')
+    return sku_query_obj
 
 
 def store_hippo(request):
-    store_hippo_data = json.loads(request.body)
+    a = datetime.datetime.now()
     api_type = request.META['HTTP_TYPE']
+    store_hippo_data = json.loads(request.body)
+    storehippo_log.info('------------API Type -' + api_type + '------------')
     status_resp = ''
-    import pdb;pdb.set_trace()
     try:
         user_obj = User.objects.get(username='storehippo')
     except:
         return HttpResponse("User Not found")
     if api_type == 'add_order':
         status_resp = create_order_storehippo(store_hippo_data, user_obj)
-    if api_type == 'add_sku':
-        status_resp = create_sku_storehippo(store_hippo_data, user_obj)
+    if api_type in ['add_sku', 'edit_sku']:
+        status_resp = create_update_sku_storehippo(store_hippo_data, user_obj)
+    b = datetime.datetime.now()
+    delta = b - a
+    time_taken = str(delta.total_seconds())
+    storehippo_log.info('------------End Time Taken in Seconds --- ' + time_taken + '-----')
     return HttpResponse(json.dumps(status_resp))
 

8432ad6f81e1c982f4f5fc73aed558f2061295f3
root <aravind@mieone.com>
2019-04-01 06:01:08 +0200

    Stock Reconciliation Mods in Tax calc

--- API_WMS/miebach/rest_api/management/commands/stock_reconciliation_script.py
+++ API_WMS/miebach/rest_api/management/commands/stock_reconciliation_script.py
@@ -273,7 +273,7 @@ class Command(BaseCommand):
                         opening_stock_check.amount_before_tax = opening_stock * stock_buy_price
                         opening_stock_check.tax_rate = (float(stock_tax_percent) * int(opening_stock_check.quantity) * float(stock_buy_price))/100
                         opening_stock_check.cess_rate = seller_po.get(sku, 0)
-                        opening_stock_check.amount_after_tax = seller_po.get(sku, 0) + opening_stock_check.cess_rate + opening_stock_check.tax_rate
+                        opening_stock_check.amount_after_tax = opening_stock_check.amount_before_tax + opening_stock_check.cess_rate + opening_stock_check.tax_rate
                         opening_stock_check.created_date = str(datetime.now().date())
                         opening_stock_check.save()
                     else:
@@ -282,7 +282,7 @@ class Command(BaseCommand):
                         opening_stock_dict['amount_before_tax'] = opening_stock * stock_buy_price
                         opening_stock_dict['tax_rate'] = (stock_tax_percent * opening_stock_dict['quantity'])/100
                         opening_stock_dict['cess_rate'] = seller_po.get(sku, 0)
-                        opening_stock_dict['amount_after_tax'] = seller_po.get(sku, 0) + opening_stock_dict['cess_rate'] + opening_stock_dict['tax_rate']
+                        opening_stock_dict['amount_after_tax'] = opening_stock_dict['amount_before_tax'] + opening_stock_dict['cess_rate'] + opening_stock_dict['tax_rate']
                         opening_stock_dict['created_date'] = str(datetime.now().date())
                         StockReconciliation.objects.create(**opening_stock_dict)
 

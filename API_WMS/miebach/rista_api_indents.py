ccba23f3fe818a9a12f2127f3540a719826b4d3f
aravindarajsekar <aravind@mieone.com>
2019-04-23 07:28:47 +0200

    Config added in Local and staging

--- API_WMS/miebach/rista_api_indents.py
+++ API_WMS/miebach/rista_api_indents.py
@@ -88,7 +88,7 @@ def sendToStockOne(resp, branch_code):
 	    int_obj = Integrations.objects.filter(**{'user':user_id, 'name':'rista', 'status':0})
 	    if not int_obj:
 		Integrations.objects.create(**{'user':user_id, 'name':'rista', 'api_instance':'rista', 'status':0, 'client_id':stockone_auth['client_id'], 'secret':stockone_auth['client_secret']})
-	    write_order_resp = writeOrders(access_token, resp)
+            write_order_resp = writeOrders(access_token, resp)
 	except:
 	    order_pull_rista_stockone_logs.info("Error in Creating Orders")
     return write_order_resp

467b237f36f35a7fcf95fb295496ac179fa4563a
mkaladhar <kaladhar@headrun.com>
2019-06-11 18:23:43 +0530

    updating the blocked quantity

--- API_WMS/miebach/rest_api/views/common.py
+++ API_WMS/miebach/rest_api/views/common.py
@@ -4799,6 +4799,7 @@ def get_styles_data(user, product_styles, sku_master, start, stop, request, cust
     else:
         product_styles_filtered = product_styles
     for product in product_styles_filtered[start: stop]:
+        prd_sku_codes = sku_master.filter(sku_class=product).only('sku_code').values_list('sku_code', flat=True)
         sku_object = sku_master.filter(user=user.id, sku_class=product)
         sku_styles = sku_object.values('image_url', 'sku_class', 'sku_desc', 'sequence', 'id'). \
             order_by('-image_url')
@@ -4810,7 +4811,6 @@ def get_styles_data(user, product_styles, sku_master, start, stop, request, cust
             total_quantity = product_styles_tot_qty_map[product]
         else:
             total_quantity = 0
-            prd_sku_codes = sku_master.filter(sku_class=product).only('sku_code').values_list('sku_code', flat=True)
             for prd_sku in prd_sku_codes:
                 total_quantity += needed_stock_data['stock_objs'].get(prd_sku, 0)
                 # total_quantity += needed_stock_data['asn_quantities'].get(prd_sku, 0)
@@ -4830,10 +4830,13 @@ def get_styles_data(user, product_styles, sku_master, start, stop, request, cust
             sku_variants[0].update(sku_spl_attrs)
             sku_styles[0]['variants'] = sku_variants
             sku_styles[0]['style_quantity'] = total_quantity
-            sku_styles[0]['asn_quantity'] = needed_stock_data['asn_quantities'].get(prd_sku, 0)
-            stock_blk_qty = needed_stock_data['enquiry_res_quantities'].get(prd_sku, 0)
-            intr_blk_qty = needed_stock_data['asn_blocked_quantities'].get(prd_sku, 0)
-            sku_styles[0]['blocked_qty'] = stock_blk_qty + intr_blk_qty
+            sku_styles[0]['asn_quantity'] = 0
+            sku_styles[0]['blocked_qty'] = 0
+            for prd_sku in prd_sku_codes:
+                sku_styles[0]['asn_quantity'] += needed_stock_data['asn_quantities'].get(prd_sku, 0)
+                stock_blk_qty = needed_stock_data['enquiry_res_quantities'].get(prd_sku, 0)
+                intr_blk_qty = needed_stock_data['asn_blocked_quantities'].get(prd_sku, 0)
+                sku_styles[0]['blocked_qty'] += stock_blk_qty + intr_blk_qty
 
             sku_styles[0]['image_url'] = resize_image(sku_styles[0]['image_url'], user)
             if style_quantities.get(sku_styles[0]['sku_class'], ''):

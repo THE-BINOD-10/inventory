eb5d34d60a71bea946dd385c390129b543aa3321
ravitejamieone <ravi@mieone.com>
2019-08-05 17:39:26 +0530

    Merge Correction

--- API_WMS/miebach/rest_api/views/common.py
+++ API_WMS/miebach/rest_api/views/common.py
@@ -9669,14 +9669,12 @@ def get_challan_number_for_dc(order , user):
     if challan_sequence:
         challan_sequence = challan_sequence[0]
         challan_num = int(challan_sequence.value)
-        order_no = challan_sequence.prefix + str(challan_num).zfill(3)
         challan_sequence.value = challan_num + 1
         challan_sequence.save()
     else:
         ChallanSequence.objects.create(marketplace='', prefix='CHN', value=1, status=1, user_id=user.id,
                                        creation_date=datetime.datetime.now())
-        order_no = '001'
-        challan_num = int(order_no)
+        challan_num = 1
     return challan_num
 
 

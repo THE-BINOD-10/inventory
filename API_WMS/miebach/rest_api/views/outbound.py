f935175a92099da04ef2faf8e50fdc3e30fe709d
ravitejamieone <ravi@mieone.com>
2019-08-07 13:41:16 +0530

    DC-SAVE -sku wise

--- API_WMS/miebach/rest_api/views/outbound.py
+++ API_WMS/miebach/rest_api/views/outbound.py
@@ -15751,13 +15751,15 @@ def generate_picklist_dc(request, user=''):
             challan_num = get_challan_number_for_dc(order , user)
             delivery_challan_dict['dc_number'] = challan_num
             invoice_data['dc_number'] = challan_num
-            delivery_challan_dict['order'] = value.values()[0].get('order','')
+            # delivery_challan_dict['order'] = value.values()[0].get('order','')
             delivery_challan_dict['picklist_number'] = picklist_number
             delivery_challan_dict['total_qty'] = total_qty
-            for val in value.values() :
-                val['order'] = ''
-            delivery_challan_dict['dcjson'] = json.dumps(value)
-            TempDeliveryChallan.objects.create(**delivery_challan_dict)
+            for dc_create in value.values():
+                delivery_challan_dict['order'] = dc_create.get('order','')
+                dc_create['order'] = ''
+                delivery_challan_dict['dcjson'] = json.dumps(dc_create)
+                TempDeliveryChallan.objects.create(**delivery_challan_dict)
+
 
     return render(request, 'templates/toggle/delivery_challan_batch_level.html', invoice_data)
 

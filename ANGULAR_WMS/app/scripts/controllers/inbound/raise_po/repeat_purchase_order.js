42277270b1923532af8ce95a623759823a8c30d1
AvinashMieone <avinash@mieone.com>
2019-08-19 18:28:24 +0530

    overrading quantity in receive po

--- ANGULAR_WMS/app/scripts/controllers/inbound/raise_po/repeat_purchase_order.js
+++ ANGULAR_WMS/app/scripts/controllers/inbound/raise_po/repeat_purchase_order.js
@@ -414,11 +414,7 @@ function ServerSideProcessingCtrl($scope, $http, $q, $state, $compile, $timeout,
           elem = $(elem).serializeArray()
           vm.common_confirm('confirm_central_po/', elem)
         } else {
-          if(!(vm.update)) {
             vm.confirm_add_po();
-          } else {
-            vm.confirm_po();
-          }
         }
       }
     }
@@ -426,7 +422,7 @@ function ServerSideProcessingCtrl($scope, $http, $q, $state, $compile, $timeout,
     vm.confirm_add_po = function() {
 
       var elem = angular.element($('form'));
-      elem = elem[0];
+      elem = elem[1];
       elem = $(elem).serializeArray();
       vm.common_confirm('confirm_add_po/', elem);
     }
@@ -436,22 +432,6 @@ function ServerSideProcessingCtrl($scope, $http, $q, $state, $compile, $timeout,
       elem = elem[1];
       elem = $(elem).serializeArray();
       vm.common_confirm('confirm_po/', elem);
-    }
-
-    vm.common_confirm = function(url, elem) {
-      var confirm_url = 'validate_wms/';
-      if (vm.warehouse_type == 'CENTRAL_ADMIN') {
-        elem.push({name:'is_central_po', value:true});
-      }
-      vm.service.apiCall(confirm_url, 'POST', elem, true).then(function(data){
-        if(data.message) {
-          if (data.data == "success") {
-            vm.raise_po(url, elem);
-          } else{
-            vm.service.pop_msg(data.data);
-          }
-        }
-      });
     }
 
     vm.common_confirm = function(url, elem) {

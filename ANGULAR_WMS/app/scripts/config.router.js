8dcf0dba80a289b8d363036310c6134cd74d698d
mkaladhar <kaladhar@headrun.com>
2019-08-20 18:54:56 +0530

    updating the prefer view

--- ANGULAR_WMS/app/scripts/config.router.js
+++ ANGULAR_WMS/app/scripts/config.router.js
@@ -2730,7 +2730,12 @@ var app = angular.module('urbanApp')
         })
           .state('user.App.Brands', {
             url: '/Brands',
-            templateUrl: 'views/outbound/app/create_orders/details.html'
+            templateUrl: 'views/outbound/app/create_orders/details.html',
+            resolve: {
+              deps: ['$ocLazyLoad', function ($ocLazyLoad) {
+                return $ocLazyLoad.load('scripts/controllers/outbound/app/settings.js');
+              }]
+            }
           })
           .state('user.App.Products', {
             url: '/Products',
@@ -2777,18 +2782,6 @@ var app = angular.module('urbanApp')
             resolve: {
               deps: ['$ocLazyLoad', function ($ocLazyLoad) {
                 return $ocLazyLoad.load('scripts/controllers/outbound/app/feedback.js');
-                      }]
-            },
-            data: {
-              title: 'feedback'
-            }
-          })
-          .state('user.App.settings', {
-            url: '/settings',
-            templateUrl: 'views/outbound/app/create_orders/settings.html',
-            resolve: {
-              deps: ['$ocLazyLoad', function ($ocLazyLoad) {
-                return $ocLazyLoad.load('scripts/controllers/outbound/app/settings.js');
                       }]
             },
             data: {

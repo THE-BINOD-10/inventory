19b4d997085cccb2cb0b1919b96e5679534ad6c6
AvinashMieone <avinash@mieone.com>
2019-11-20 18:11:13 +0530

    change of stock sync in confirmations

--- API_WMS/miebach/rest_api/views/outbound.py
+++ API_WMS/miebach/rest_api/views/outbound.py
@@ -2393,6 +2393,7 @@ def picklist_confirmation(request, user=''):
                     auto_skus.append(val['wms_code'])
         if auto_skus:
             auto_skus = list(set(auto_skus))
+            if user.username in MILKBASKET_USERS: check_and_update_marketplace_stock(auto_skus, user)
             price_band_flag = get_misc_value('priceband_sync', user.id)
             if price_band_flag == 'true':
                 reaches_order_val, sku_qty_map = check_req_min_order_val(user, auto_skus)
@@ -2404,7 +2405,6 @@ def picklist_confirmation(request, user=''):
             else:
                 auto_po(auto_skus, user.id)
         detailed_invoice = get_misc_value('detailed_invoice', user.id)
-
     	#Check DM Rista User
     	int_obj = Integrations.objects.filter(**{'user':user.id, 'name':'rista', 'status':0})
     	if int_obj and rista_order_id_list:
@@ -2435,7 +2435,6 @@ def picklist_confirmation(request, user=''):
 
                 invoice_data['order_id'] = invoice_data['order_id']
                 user_profile = UserProfile.objects.get(user_id=user.id)
-                if user.username in MILKBASKET_USERS: check_and_update_marketplace_stock(auto_skus, user)
                 if not invoice_data['detailed_invoice'] and invoice_data['is_gst_invoice']:
                     invoice_data = build_invoice(invoice_data, user, False)
                     #return HttpResponse(json.dumps({'data': invoice_data, 'message': '',

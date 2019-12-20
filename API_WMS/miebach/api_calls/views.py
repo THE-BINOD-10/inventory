ed70fd33ede5dc9e64d8ec0b6d192eb960d02bb9
AvinashMieone <avinash@mieone.com>
2019-12-20 14:51:13 +0530

    taxes in get_skus

--- API_WMS/miebach/api_calls/views.py
+++ API_WMS/miebach/api_calls/views.py
@@ -980,6 +980,13 @@ def get_skus(request):
     sku_records = page_info['data']
     for sku in sku_records:
         updated = ''
+        cgst, sgst, igst, cess = '','','',''
+        tax_obj = TaxMaster.objects.filter(product_type=sku.product_type, user=user.id)
+        if tax_obj:
+            cgst = str(tax_obj[0].cgst_tax)
+            sgst = str(tax_obj[0].sgst_tax)
+            igst = str(tax_obj[0].igst_tax)
+            cess = str(tax_obj[0].cess_tax)
         if sku.updation_date:
             updated = sku.updation_date.strftime('%Y-%m-%d %H:%M:%S')
         data_dict = OrderedDict(( ('id', sku.id), ('sku_code', sku.sku_code), ('sku_desc', sku.sku_desc),
@@ -988,7 +995,12 @@ def get_skus(request):
                                   ('cost_price', str(sku.cost_price)),
                                   ('product_type', sku.product_type),
                                   ('hsn_code', sku.hsn_code),
+                                  ('cgst', cgst),
+                                  ('sgst', sgst),
+                                  ('igst', igst),
+                                  ('cess', cess),
                                   ('ean_number', sku.ean_number),
+                                  ('measurement_type', sku.measurement_type),
                                   ('active', sku.status),
                                   ('created_at', sku.creation_date.strftime('%Y-%m-%d %H:%M:%S')),
                                   ('updated_at', updated )))

1df9dbcbedf9fc7e5d82e7f4fee7c1e923d81f30
Avadhanisharma <avadhani@headrun.com>
2020-02-10 11:42:00 +0530

    adding code for ba tosa

--- API_WMS/miebach/rest_api/views/stock_locator.py
+++ API_WMS/miebach/rest_api/views/stock_locator.py
@@ -3494,7 +3494,7 @@ def ba_to_sa_calculate_now(request, user=''):
                                     sku_avg_sale_mapping.values())
             sum_avg_more_sales = 0
             for avg_more_sale in avg_more_sales:
-                sum_avg_more_sales += avg_more_sale['sku_avg_sale_per_day_units']
+                sum_avg_more_sales += avg_more_sale['avg_sale_per_day_units']
             cumulative_contribution = (sum_avg_more_sales / total_avg_sale_per_day_unit) * 100
             if cumulative_contribution <= 40:
                 classification = 'Fast'

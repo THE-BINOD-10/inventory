65d51f1765d723ee40f8995f95fa2cd1be5d1481
AvinashMieone <avinash@mieone.com>
2020-02-20 10:55:14 +0530

    .

--- API_WMS/miebach/rest_api/views/miebach_utils.py
+++ API_WMS/miebach/rest_api/views/miebach_utils.py
@@ -5000,13 +5000,11 @@ def tally_dump(invoice_amount_picked,unit_price_inclusive_tax, gst_number,unit_d
     discount_percent, selling_price = 0, 0
     cgst_amount, sgst_amount, igst_amount = 0,0,0
     tally_Data = OrderedDict()
-    try:
-        discount = unit_discount*quantity
+    discount = unit_discount*quantity
+    if data['unit_price']:
         discount_percent = (discount*100)/(quantity*float(data['unit_price']))
         unit_min_dis = float(data['unit_price']) - unit_discount
         selling_price = (unit_min_dis)+((unit_min_dis)*(tax_percent/100))
-    except:
-        discount_percent, selling_price = 0, 0
     amt = unit_price_inclusive_tax * float(quantity) - discount
     if order_summary:
       cgst_amount = float(order_summary[0].cgst_tax) * (float(amt) / 100)

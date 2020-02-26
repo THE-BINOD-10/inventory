77cd30a1a8be3aaa5e97aaa8f0de6d8b40191480
bharath <bharath@mieone.com>
2020-02-26 23:15:44 +0530

    adding code for ebway bill

--- API_WMS/miebach/rest_api/views/miebach_utils.py
+++ API_WMS/miebach/rest_api/views/miebach_utils.py
@@ -4893,7 +4893,7 @@ def get_order_summary_data(search_params, user, sub_user):
                             invoice_number = str(invoice_number_obj[0].seller_order.order.order_id)
             if invoice_number:
                 shipment_info = ShipmentInfo.objects.filter(order__user=user.id,order__original_order_id=data['original_order_id'],
-                                                            invoice_number=invoice_number)
+                                                            invoice_number__endswith='/'+invoice_number)
                 if shipment_info.exists():
                     ewaybill_number = str(shipment_info[0].order_shipment.ewaybill_number)
 

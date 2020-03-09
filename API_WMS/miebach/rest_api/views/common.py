ec6ce4965fd09160ffc432fd5e1a03c01f635e18
Headrun <chandu@headrun.com>
2020-03-09 16:49:34 +0530

    Converting the float to int format in sku packs

--- API_WMS/miebach/rest_api/views/common.py
+++ API_WMS/miebach/rest_api/views/common.py
@@ -3697,7 +3697,7 @@ def common_calculations(arg_data):
     quantity = get_decimal_limit(user.id ,quantity)
     invoice_amount = get_decimal_limit(user.id ,invoice_amount ,'price')
     if sku_packs:
-        sku_packs = total_quantity //sku_packs
+        sku_packs = int(quantity //sku_packs)
     else:
         sku_packs= 0
 

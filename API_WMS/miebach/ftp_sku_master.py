094e6a113a19661e5153e3899822faffaa61d2c3
AvinashMieone <avinash@mieone.com>
2020-03-23 23:46:16 +0530

    #2734 mandatory fields and price check

--- API_WMS/miebach/ftp_sku_master.py
+++ API_WMS/miebach/ftp_sku_master.py
@@ -10,13 +10,72 @@ HERE = os.path.abspath(os.path.dirname(__file__))
 DATA_DIR = os.path.abspath(os.path.join(HERE))
 
 
+def validate_(df, file_split):
+    df['error_status'] = df.isna().dot(df.columns + '-should not be empty, ').str.rstrip(', ')
+    price_check=pd.to_numeric(df.Price, errors='coerce')
+    df['price_status'] = price_check.isna()
+    price_dict = {True: 'Price Must Be Number', False: ''}
+    df['price_status'] = df['price_status'].replace(price_dict)
+
+    mrp_check=pd.to_numeric(df.MRP, errors='coerce')
+    df['mrp_status'] = price_check.isna()
+    price_dict = {True: 'MRP Must Be Number', False: ''}
+    df['mrp_status'] = df['mrp_status'].replace(price_dict)
+
+    if len(set(df['error_status'])) > 1 or len(set(df['mrp_status'])) > 1 or  len(set(df['price_status'])) >1 :
+        if 'xl' in file_split[1]:
+            file_name = file_split[0]+'error.xlsx'
+            df.to_excel(file_name)
+        else:
+            file_name = file_split[0]+'error.csv'
+            df.to_csv(file_name)
+        file = open(file_name,'rb')
+        ftp.cwd('Failed')
+        ftp.storbinary(('STOR %s')%file_name, file)
+        ftp.cwd('..')
+        file.close()
+        return False
+    else:
+        return True
+
+def validate_sku_code(df):
+    sku_code = df['Part No']
+    sku_code = sku_code.lstrip()
+    sku_code = sku_code.rstrip()
+    if df['Brand'].lower() == 'toyota':
+        if len(sku_code) > 10 :
+            if isinstance(sku_code[10], int):
+                return sku_code[:10]
+            else:
+                return sku_code[:11]
+        else:
+            return sku_code
+    elif df['Brand'].lower() == 'honda':
+        if len(sku_code) >= 8 :
+            sku_code = sku_code[:5]+'-'+sku_code[5:8]+'-'+sku_code[8:]
+            return sku_code
+        else:
+            return sku_code
+    else:
+        return sku_code
+
 def make_df_from_excel(file_name, nrows):
     file_path = os.path.abspath(os.path.join(DATA_DIR, file_name))
-    xl = pd.ExcelFile(file_path)
-    sheetname = xl.sheet_names[0]
-    df_header = pd.read_excel(file_path, sheetname=sheetname, nrows=1)
+    # xl = pd.ExcelFile(file_path)
+    # sheetname = xl.sheet_names[0]
+    file_split = file_name.split('.')
+    if 'xl' in file_split[1]:
+        df_header = pd.read_excel(file_path, nrows=1)
+        df = pd.read_excel(file_path)
+    else:
+        df_header = pd.read_csv(file_path, nrows=1)
+        df = pd.read_csv(file_path)
+    st = validate_(df,file_split[0])
+    if not st:
+        return False
     gomech_users = ['gomechanic_admin','gomechanic_gurgaon', 'gomechanic_mumbai', 'gomechanic_bangalore']
     for username in gomech_users:
+        print(username)
         user = User.objects.get(username=username)
         count = 0
         i_chunk = 0
@@ -25,9 +84,11 @@ def make_df_from_excel(file_name, nrows):
         product_types = list(TaxMaster.objects.filter(user_id=user.id).values_list('product_type', flat=True).distinct())
         while True:
             chunks = []
-            df_chunk = pd.read_excel(
-                file_path, sheetname=sheetname,
-                nrows=nrows, skiprows=skiprows, header=None)
+            try:
+                df_chunk = pd.read_excel(file_path,nrows=nrows, skiprows=skiprows, header=None)
+            except:
+                df_chunk = pd.read_csv(file_path,nrows=nrows, skiprows=skiprows, header=None)
+            # df_chunk['sku_code']=df_chunk.apply(validate_sku_code,axis=1)
             skiprows += nrows
             if not df_chunk.shape[0]:
                 break
@@ -38,12 +99,11 @@ def make_df_from_excel(file_name, nrows):
             df_chunks = pd.concat(chunks)
             columns = {i: col for i, col in enumerate(df_header.columns.tolist())}
             df_chunks.rename(columns=columns, inplace=True)
-            df = pd.concat([df_header, df_chunks])
+            # df = pd.concat([df_header, df_chunks])
             data_dict = {"user":user.id}
-            for index,row in df.iterrows():
+            for index,row in df_chunks.iterrows():
                 str_dict = {'hsn_code':'HSN Code', 'sku_desc':'Part Desc'}
                 num_dict = {'price':'Price', 'mrp':'MRP'}
-                # df.to_csv('test.csv', index=index, header=df.columns.values,  mode='a')
                 if row.get('Part No', ''):
                     sku_code = row.get('Part No')
                     if isinstance(sku_code, float):
@@ -92,18 +152,29 @@ def make_df_from_excel(file_name, nrows):
                     sku_master = SKUMaster(**data_dict)
                     sku_master.save()
                     sku_data = sku_master
+    return True
             
 ftp.cwd(mypath)
+wrapper = """<html>
+    <head>
+    </head>
+    <body><p>%s</p></body>
+    </html>"""
 names = ftp.nlst()
+mail_id = ['avinash@mieone.com']
 for name in names:
     ftp.retrbinary("RETR " + name, open(name, 'wb').write)
     existingFilepath = "Uploads/"+name
     newFilepath = "Processing/"+name
+    send_mail(mail_id, 'FTP file Upload', wrapper %("SKU Upload started for the file "+name))
     ftp.cwd('..')
     success = ftp.rename(existingFilepath,newFilepath)
     com_existingFilepath = "Processing/"+name
     com_newFilepath = "Completed/"+name
-    make_df_from_excel(name, nrows=5)
-    success = ftp.rename(com_existingFilepath,com_newFilepath)
+    status = make_df_from_excel(name, nrows=5)
+    if status:
+        send_mail(mail_id, 'FTP file Upload', wrapper %("SKU Upload Completed for the file "+name))
+        os.remove(name)
+        success = ftp.rename(com_existingFilepath,com_newFilepath)
     ftp.cwd(mypath)
 ftp.quit()

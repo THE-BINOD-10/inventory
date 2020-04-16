b6ffd0d4d5a807cc03f6424e2efc6ca5e8bab0a9
Headrun <chandu@headrun.com>
2020-04-16 16:06:20 +0530

    Remove the space

--- API_WMS/miebach/rest_api/views/miebach_utils.py
+++ API_WMS/miebach/rest_api/views/miebach_utils.py
@@ -8294,7 +8294,7 @@ def get_stock_transfer_report_data(search_params, user, sub_user):
                     ord_dict = OrderedDict(
                         (('Date', date), ('Order ID', data.order_id), ('Invoice Number', invoice_number),
                          ('Source Warehouse', user.username), ('Destination Warehouse', destination),
-                         ('SKU Code', da   ta.sku.sku_code), ('SKU Description', data.sku.sku_desc),
+                         ('SKU Code', data.sku.sku_code), ('SKU Description', data.sku.sku_desc),
                          ('Order Quantity', quantity), ('Order Amount(w/o tax)', order_wo_amount),
                          ('Order Tax Amount', order_tax_amount), ('Total Order Amount', total_order_amount),
                          ('Unit Price', price), ('Tax Percentage', tax_percentage),

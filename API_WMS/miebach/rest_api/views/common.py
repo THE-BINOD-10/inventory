10b57d973205380a85c41c7deaed876129c7481b
Avadhani <Avadhani>
2020-04-28 23:10:24 +0530

    #3246 fixing issues

--- API_WMS/miebach/rest_api/views/common.py
+++ API_WMS/miebach/rest_api/views/common.py
@@ -9572,19 +9572,21 @@ def update_substitution_data(src_stocks, dest_stocks, src_sku, src_loc, src_qty,
 
 
 def update_stock_detail(stocks, quantity, user, rtv_id):
-    rtv_quantity = quantity
     for stock in stocks.iterator():
         if stock.quantity > quantity:
             stock.quantity -= quantity
             seller_stock = stock.sellerstock_set.filter()
             if seller_stock.exists():
                 change_seller_stock(seller_stock[0].seller_id, stock, user, quantity, 'dec')
+            save_sku_stats(user, stock.sku.id, rtv_id, 'rtv', quantity, stock)
             quantity = 0
             if stock.quantity < 0:
                 stock.quantity = 0
             stock.save()
         elif stock.quantity <= quantity:
             quantity -= stock.quantity
+            rtv_quantity = stock.quantity
+            save_sku_stats(user, stock.sku.id, rtv_id, 'rtv', rtv_quantity, stock)
             seller_stock = stock.sellerstock_set.filter()
             if seller_stock.exists():
                 change_seller_stock(seller_stock[0].seller_id, stock, user, stock.quantity, 'dec')
@@ -9592,7 +9594,7 @@ def update_stock_detail(stocks, quantity, user, rtv_id):
             stock.save()
         if quantity == 0:
             break
-    save_sku_stats(user, stock.sku.id, rtv_id, 'rtv', rtv_quantity, stock)
+
 
 
 def reduce_location_stock(cycle_id, wmscode, loc, quantity, reason, user, pallet='', batch_no='', mrp='',price ='',

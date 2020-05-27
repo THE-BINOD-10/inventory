2932b5a8366d83588b14067b4438e6d979f02b1d
AvinashMieone <avinash@mieone.com>
2020-05-27 18:35:25 +0530

    #3402 PR Integration after the final approval, Supplier creation handling multiple address with GST

--- API_WMS/miebach/miebach_admin/models.py
+++ API_WMS/miebach/miebach_admin/models.py
@@ -289,7 +289,9 @@ class SupplierMaster(models.Model):
     markdown_percentage = models.FloatField(default=0)
     ep_supplier = models.IntegerField(default=0)
     reference_id = models.CharField(max_length=64, default='')
-    payment_terms = models.CharField(max_length=24, default='')
+    payment_terms = models.CharField(max_length=256, default='')
+    subsidiary = models.CharField(max_length=64, default='')
+    place_of_supply = models.CharField(max_length=64, default='')
     class Meta:
         db_table = 'SUPPLIER_MASTER'
         index_together = ('name', 'user')

2b7d0443f7a8afccfa5dad389ce30b2479cd14f2
mkaladhar <kaladhar@mieone.com>
2020-06-02 08:51:24 +0530

    reslove ui error message

--- ANGULAR_WMS/app/scripts/controllers/inbound/credit_note.js
+++ ANGULAR_WMS/app/scripts/controllers/inbound/credit_note.js
@@ -167,7 +167,9 @@ function ServerSideProcessingCtrl($scope, $http, $state, $timeout, Session, DTOp
             vm.close();
             vm.reloadData();
           } else {
-            Service.showNoty(data.data);
+            Service.showNoty('Success');
+            vm.close();
+            vm.reloadData();
           }
         })
       }

45658f9d08626fcbf28d99cae7dea1c47677b1a5
siddique4 <abu@mieone.com>
2020-06-11 14:55:51 +0530

    filters in django admin

--- API_WMS/miebach/stockone_integrations/admin.py
+++ API_WMS/miebach/stockone_integrations/admin.py
@@ -10,4 +10,5 @@ from models import IntegrationMaster
 @admin.register(IntegrationMaster)
 class IntegrationMasterAdmin(admin.ModelAdmin):
     search_fields = ['user', 'integration_type', 'stockone_reference', 'integration_reference']
-    list_display = ['user', 'integration_type', 'stockone_reference', 'integration_reference', 'creation_date', 'updation_date']
+    list_display = ['user', 'integration_type', 'stockone_reference', 'integration_reference', 'status', 'creation_date', 'updation_date']
+    list_filter = ('integration_type', 'status')

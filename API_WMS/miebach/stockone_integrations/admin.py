d911bf7e4763feb2531107b4379ffb24c88cd171
siddique4 <abu@mieone.com>
2020-06-13 00:38:22 +0530

    adding execute all action to integration queue

--- API_WMS/miebach/stockone_integrations/admin.py
+++ API_WMS/miebach/stockone_integrations/admin.py
@@ -7,8 +7,19 @@ from models import IntegrationMaster
 
 # Register your models here.
 
+from automate import runStoredAutomatedTasks
+
 @admin.register(IntegrationMaster)
 class IntegrationMasterAdmin(admin.ModelAdmin):
     search_fields = ['user', 'integration_type', 'stockone_reference', 'integration_reference']
     list_display = ['user', 'integration_type', 'stockone_reference', 'integration_reference', 'module_type', 'status', 'creation_date', 'updation_date']
     list_filter = ('integration_type', 'status')
+
+    actions = ["execute_queue"]
+
+    def execute_queue(self, request, queryset):
+        runStoredAutomatedTasks()
+
+    execute_queue.short_description = "Execute Queue"
+
+

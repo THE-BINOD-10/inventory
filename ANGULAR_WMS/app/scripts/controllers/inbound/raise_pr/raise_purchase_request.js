5d912e7542bd933aa99fbc0f4fdd2fddffcabcd8
SaiPavan <sai@mieone.com>
2020-06-15 16:28:57 +0530

    #3760 1.Category select bug fix, 2.SentDOA flag bug fix when closed without send for approval 3.Consolidated Preview selecting different product categories bug fix

--- ANGULAR_WMS/app/scripts/controllers/inbound/raise_pr/raise_purchase_request.js
+++ ANGULAR_WMS/app/scripts/controllers/inbound/raise_pr/raise_purchase_request.js
@@ -337,6 +337,7 @@ function ServerSideProcessingCtrl($scope, $http, $q, $state, $rootScope, $compil
           vm.model_data["receipt_types"] = data.data.receipt_types;
           vm.model_data.prodcatg_map = data.data.prodcatg_map;
           vm.model_data.product_categories = Object.keys(vm.model_data.prodcatg_map);
+          vm.model_data.sku_categories = vm.model_data.prodcatg_map[vm.model_data.product_categories[0]];
           angular.forEach(seller_data, function(seller_single){
               vm.model_data.seller_types.push(seller_single.id + ':' + seller_single.name);
           });
@@ -399,6 +400,7 @@ function ServerSideProcessingCtrl($scope, $http, $q, $state, $rootScope, $compil
 
     vm.reset_model_data = function(product_category){
       vm.model_data.data = [];
+      vm.model_data.sku_category = "";
       var emptylineItems = {"wms_code":"", "ean_number": "", "order_quantity":"", "price":0,
                             "measurement_unit": "", "row_price": 0, "tax": "", "is_new":true,
                             "sgst_tax": "", "cgst_tax": "", "igst_tax": "", "utgst_tax": "",
@@ -547,26 +549,27 @@ function ServerSideProcessingCtrl($scope, $http, $q, $state, $rootScope, $compil
     vm.pr_to_po_preview = function(){
       vm.bt_disable = true;
       var prIds = [];
-      var firstDeptType = ''
-      var firstProdCatg = ''
+      var deptTypes = [];
+      var prodCatgs = [];
 
       angular.forEach(vm.selected, function(value, key) {
         if(value) {
           var temp = vm.dtInstance.DataTable.context[0].aoData[Number(key)];
-          if (firstDeptType.length == 0){
-            firstDeptType = temp['_aData']['Department Type'];
-            firstProdCatg = temp['_aData']['Product Category'];
-            prIds.push(temp['_aData']["Purchase Id"]);
-          } else {
-            if (firstDeptType != temp['_aData']['Department Type'] || firstProdCatg != temp['_aData']['Product Category']) {
-              vm.service.showNoty("Same Department/ProductCategory PRs can be consolidated");
-              prIds = [];
-            } else {
-              prIds.push(temp['_aData']["Purchase Id"]);
-            }
+          var deptType = temp['_aData']['Department Type'];
+          var prodCatg = temp['_aData']['Product Category'];
+          prIds.push(temp['_aData']["Purchase Id"]);
+          if (!deptTypes.includes(deptType)){
+            deptTypes.push(deptType);
+          }
+          if (!prodCatgs.includes(prodCatg)){
+            prodCatgs.push(prodCatg);
           }
         }
         if(Object.keys(vm.selected).length-1 == parseInt(key)){
+          if (deptTypes.length > 1 || prodCatgs.length > 1) {
+            prIds = [];
+            vm.service.showNoty("Same Department/ProductCategory PRs can be consolidated");
+          }
           var data_dict = {
             'prIds': JSON.stringify(prIds)
           };
@@ -574,9 +577,6 @@ function ServerSideProcessingCtrl($scope, $http, $q, $state, $rootScope, $compil
             vm.service.apiCall('get_pr_preview_data/', 'POST', data_dict, true).then(function(data){
               if(data.message){
                 vm.preview_data = data.data;
-                // for(var i = 0; i < vm.preview_data.data.length; i++) {
-                //   vm.preview_data.data[i].supplier_id_name = vm.preview_data.data[i]['supplierDetails'][0].supplier_id + ":" + vm.preview_data.data[i]['supplierDetails'][0].supplier_name;
-                // }
                 $state.go("app.inbound.RaisePr.PRemptyPreview");
               }
             });
@@ -648,7 +648,9 @@ function ServerSideProcessingCtrl($scope, $http, $q, $state, $rootScope, $compil
         }
       });
       modalInstance.result.then(function (selectedItem) {
-        lineItem.is_doa_sent = true;
+        if (selectedItem){
+          lineItem.is_doa_sent = true;          
+        }
         vm.display_vision = {'display': 'block'};
         console.log(selectedItem);
       });
@@ -1389,7 +1391,7 @@ angular.module('urbanApp').controller('skuSupplierCtrl', function ($scope, $http
     vm.service.apiCall('send_supplier_doa/', 'POST', vm.model_data, true).then(function(data){
       if(data.message) {
         if(data.data == "Added Successfully") {
-          vm.close();
+          vm.close('data');
         } else {
           vm.service.pop_msg(data.data);
         }
@@ -1406,7 +1408,7 @@ angular.module('urbanApp').controller('skuSupplierCtrl', function ($scope, $http
     })
   }
   vm.get_sku_mrp(vm.requestData['sku_code']);
-  vm.close = function () {
-    $modalInstance.close('data');
+  vm.close = function (value) {
+    $modalInstance.close(value);
   };
 });

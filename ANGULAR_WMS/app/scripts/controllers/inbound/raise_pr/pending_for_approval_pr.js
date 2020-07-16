d8d7b4f9d1de134c985ca45fd8d30a747800b3c4
mkaladhar <kaladhar@mieone.com>
2020-07-16 21:43:48 +0530

    #4103 finished HSN Validation, pr number notify, email double pop-up

--- ANGULAR_WMS/app/scripts/controllers/inbound/raise_pr/pending_for_approval_pr.js
+++ ANGULAR_WMS/app/scripts/controllers/inbound/raise_pr/pending_for_approval_pr.js
@@ -304,7 +304,9 @@ function ServerSideProcessingCtrl($scope, $http, $q, $state, $rootScope, $compil
     }
     if ($rootScope.$current_pr != '') {
       vm.supplier_id = $rootScope.$current_pr['Supplier ID'];
-      vm.dynamic_route($rootScope.$current_pr);
+      vm.current_pr = $rootScope.$current_pr
+      vm.dynamic_route(vm.current_pr);
+      $rootScope.$current_pr = '';
     }
     vm.base = function() {
       vm.title = "Raise PR";
@@ -692,51 +694,6 @@ function ServerSideProcessingCtrl($scope, $http, $q, $state, $rootScope, $compil
         vm.display_vision = {'display': 'block'};
         console.log(selectedItem);
       });
-    }
-    
-    vm.convert_pr_to_po = function(form) {
-      var selectedItems = [];
-      var alertMsg = "";
-      angular.forEach(vm.preview_data.data, function(eachLineItem){
-        if (eachLineItem.checkbox){
-          if (eachLineItem.product_category == 'Kits&Consumables' && 
-                (Object.keys(eachLineItem.supplierDetails).length == 0)){
-            vm.service.showNoty("Supplier Should be present for Kits&Consumables");
-          } else {
-            if (eachLineItem.moq > eachLineItem.quantity){
-              alertMsg = alertMsg + " " + eachLineItem.sku_code 
-            } else {
-              selectedItems.push({name: "sku_code", value: eachLineItem.sku_code});
-              selectedItems.push({name: 'pr_id', value:eachLineItem.pr_id});
-              selectedItems.push({name: 'supplier', value: eachLineItem.supplier_id});
-              selectedItems.push({name: 'quantity', value: eachLineItem.quantity});
-            };
-          }
-        }
-      });
-      if (selectedItems.length == 0){
-        vm.service.showNoty("Either Items not selected or quantiy not met MOQ Quantity for selected.")
-      }
-      var finalAlerMsg = '';
-      if (alertMsg) {
-        finalAlerMsg = alertMsg+" - Can't be processed";
-      }
-      if (selectedItems.length > 0){
-        vm.service.alert_msg(finalAlerMsg).then(function(msg) {
-          if (msg == "true") {
-            vm.service.apiCall('convert_pr_to_po/', 'POST', selectedItems, true).then(function(data){
-            if(data.message){
-                if(data.data == 'Converted PR to PO Successfully') {
-                  vm.close();
-                  vm.service.refresh(vm.dtInstance);
-                } else {
-                  vm.service.pop_msg(data.data);
-                }
-              }
-            })
-          }
-        })
-      }
     }
 
     vm.print_pending_po = function(form, validation_type) {

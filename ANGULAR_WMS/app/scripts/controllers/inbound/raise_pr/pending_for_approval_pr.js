b9a41603546557a885091b5fff402025f6de751a
mkaladhar <kaladhar@mieone.com>
2020-07-22 05:36:41 +0530

    updating the values

--- ANGULAR_WMS/app/scripts/controllers/inbound/raise_pr/pending_for_approval_pr.js
+++ ANGULAR_WMS/app/scripts/controllers/inbound/raise_pr/pending_for_approval_pr.js
@@ -497,7 +497,7 @@ function ServerSideProcessingCtrl($scope, $http, $q, $state, $rootScope, $compil
       if (validation_type == "approved" && vm.permissions.change_pendinglineitems) {
         for (var i = 0; i < data.length; i++) {
           var compare_gstin = vm.model_data.data[i].fields.supplierDetails[vm.model_data.data[i].fields.supplier_id_name].gstin
-          if (compare_gstin != '' || compare_gstin != 0) {
+          if ((compare_gstin != '' || compare_gstin != 0) && vm.model_data.data[i].fields['order_quantity'] > 0) {
             if (!vm.model_data.data[i].fields.hsn_code || vm.model_data.data[i].fields.hsn_code == 0) {
               status = false;
               sku_list = sku_list + ', ' + vm.model_data.data[i].fields.sku.wms_code;

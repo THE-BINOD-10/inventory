fc6caeb1a28770bb40431514bdec8d11e0d9bf66
Avadhani <Avadhani>
2020-07-24 11:57:15 +0530

    adding tax type

--- API_WMS/miebach/rest_api/views/uploads.py
+++ API_WMS/miebach/rest_api/views/uploads.py
@@ -9862,12 +9862,12 @@ def validate_tax_master_form(request, reader, user, no_of_rows, no_of_cols, fnam
                     else:
                         index_status.setdefault(row_idx, set()).add('Enter Minimum Amount')
                 elif key in ['sgst_tax', 'cgst_tax','igst_tax',]:
-                    if data_dict.get('tax_type','').lower() == 'inter state' and key == 'igst':
+                    if data_dict.get('tax_type','').lower() == 'inter_state' and key == 'igst':
                         if not cell_data and cell_data != 0:
                             index_status.setdefault(row_idx, set()).add('Fill the IGST Column for the inter state')
                         elif not isinstance(cell_data , float):
                             index_status.setdefault(row_idx, set()).add('Fill the IGST Column for the inter state')
-                    elif data_dict.get('tax_type','').lower() == 'intra state' and key in ['cgst' ,'sgst']:
+                    elif data_dict.get('tax_type','').lower() == 'intra_state' and key in ['cgst' ,'sgst']:
                         if not cell_data and cell_data != 0:
                             index_status.setdefault(row_idx, set()).add('Fill the SGST and CGST Column for the intra state')
                         elif not isinstance(cell_data , float):

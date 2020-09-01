d5d5fceee830324d7139e88efceda6405bb8f1f3
AvinashMieone <avinash@mieone.com>
2020-09-01 12:15:48 +0530

    #4423 attune_id(orgid) and testcode validation

--- API_WMS/miebach/rest_api/management/commands/consumption_api_script.py
+++ API_WMS/miebach/rest_api/management/commands/consumption_api_script.py
@@ -27,49 +27,54 @@ def init_logger(log_file):
 log = init_logger('logs/metropolis_consumption.log')
 
 def update_consumption(consumption_obj, user):
-	if consumption_obj and consumption_obj.get('STATUS_CODE', 0) == 200:
-		status_time = consumption_obj.get('TIME', 0)
-		if consumption_obj.keys() > 2:
-			consumption_lis = consumption_obj.keys()
-			consumption_lis.remove('STATUS_CODE')
-			consumption_lis.remove('TIME')
-			for key in consumption_lis:
-				consumption_dict = consumption_obj[key]
-				test_code = consumption_dict.get('TCode', '')
-				test_name = consumption_dict.get('TNAME', '')
-				name = consumption_dict.get('NAME', '')
-				orgid = consumption_dict.get('OrgID', '')
-				data_dict = {'user':user}
-				number_dict = {'total_test':'TT', 'one_time_process':'P1', 'two_time_process':'P2','three_time_process':'P3' ,
-				'n_time_process':'PN', 'rerun':'RR', 'quality_check':'Q', 'total_patients':'TP', 'total':'T', 'no_patient':'NP',
-				'qnp':'QNP', 'patient_samples': 'PatientSamples'}
-				for key, value in number_dict.iteritems():
-					data_dict[key] = 0
-					if consumption_dict.get(value, 0):
-						data_dict[key] = float(consumption_dict.get(value, 0))
-				sum_ = data_dict['one_time_process'] + data_dict['two_time_process'] + data_dict['three_time_process'] + data_dict['quality_check'] +data_dict['no_patient']
-				diff = data_dict['total_test'] - sum_
-				if diff and data_dict['n_time_process']:
-					n_time_process_val = diff/data_dict['n_time_process']
-				data_dict['n_time_process_val'] = n_time_process_val
-				try:
-					Consumption.objects.create(**data_dict)
-				except:
-					log.info("Consumption creation failed for %s and data_dict was %s" % str(user.username), str(data_dict))
+    if consumption_obj and consumption_obj.get('STATUS_CODE', 0) == 200:
+        status_time = consumption_obj.get('TIME', 0)
+        if consumption_obj.keys() > 2:
+            consumption_lis = consumption_obj.keys()
+            consumption_lis.remove('STATUS_CODE')
+            consumption_lis.remove('TIME')
+            for key in consumption_lis:
+                consumption_dict = consumption_obj[key]
+                test_code = consumption_dict.get('TCode', '')
+                if test_code:
+                    test_obj = TestMaster.objects.filter(test_code=str(test_code))
+                    if test_obj.exists():
+                        data_dict['test'] = test_obj[0]
+                test_name = consumption_dict.get('TNAME', '')
+                name = consumption_dict.get('NAME', '')
+                orgid = consumption_dict.get('OrgID', '')
+                data_dict = {'user':user}
+                number_dict = {'total_test':'TT', 'one_time_process':'P1', 'two_time_process':'P2','three_time_process':'P3' ,
+                'n_time_process':'PN', 'rerun':'RR', 'quality_check':'Q', 'total_patients':'TP', 'total':'T', 'no_patient':'NP',
+                'qnp':'QNP', 'patient_samples': 'PatientSamples'}
+                for key, value in number_dict.iteritems():
+                    data_dict[key] = 0
+                    if consumption_dict.get(value, 0):
+                        data_dict[key] = float(consumption_dict.get(value, 0))
+                sum_ = data_dict['one_time_process'] + data_dict['two_time_process'] + data_dict['three_time_process'] + data_dict['quality_check'] +data_dict['no_patient']
+                diff = data_dict['total_test'] - sum_
+                if diff and data_dict['n_time_process']:
+                    n_time_process_val = diff/data_dict['n_time_process']
+                data_dict['n_time_process_val'] = n_time_process_val
+                try:
+                    Consumption.objects.create(**data_dict)
+                except:
+                    log.info("Consumption creation failed for %s and data_dict was %s" % str(user.username), str(data_dict))
 
 
 class Command(BaseCommand):
     help = "Consumption data"
 
     def handle(self, *args, **options):
-    	user = User.objects.get(username='demo')
-    	self.stdout.write("Started Consumption call")
-    	integrations = Integrations.objects.filter(user=user.id, status=1, name='metropolis')
-    	for integrate in integrations:
-    		obj = eval(integrate.api_instance)(company_name=integrate.name, user=user)
-    		today = datetime.date.today().strftime('%Y%m%d')
-    		data = {'fromdate':today, 'todate':today, 'orgid':67}
-    		consumption_obj = obj.get_consumption_data(data=data,user=user)
-    		update_consumption(consumption_obj, user)
-    	log.info("succesfull Consumption call for %s" % user.username)
-    	self.stdout.write("completed Consumption call")
+        users = User.objects.filter().exclude(userprofile__attune_id=None)
+        self.stdout.write("Started Consumption call")
+        for user in users:
+            integrations = Integrations.objects.filter(user=user.id, status=1, name='metropolis')
+            for integrate in integrations:
+                obj = eval(integrate.api_instance)(company_name=integrate.name, user=user)
+                today = datetime.date.today().strftime('%Y%m%d')
+                data = {'fromdate':today, 'todate':today, 'orgid':67}
+                consumption_obj = obj.get_consumption_data(data=data,user=user)
+                update_consumption(consumption_obj, user)
+            log.info("succesfull Consumption call for %s" % user.username)
+        self.stdout.write("completed Consumption call")

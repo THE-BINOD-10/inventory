1c08c5aa1ca1021e85153d686c9597e829c25196
mkaladhar <kaladhar@mieone.com>
2020-12-24 18:56:58 +0530

    updating the view orders pages

--- API_WMS/miebach/rest_api/views/outbound.py
+++ API_WMS/miebach/rest_api/views/outbound.py
@@ -15300,8 +15300,11 @@ def get_stock_transfer_order_level_data(start_index, stop_index, temp_data, sear
 
     for data in master_data[start_index:stop_index]:
         checkbox = '<input type="checkbox" name="order_id" value="%s">' % data['order_id']
+        source_name = User.objects.get(username=data['st_po__open_st__warehouse__username'])
         warehouse = User.objects.get(id=data['st_po__open_st__sku__user'])
         temp_data['aaData'].append({'': checkbox, 'Warehouse Name': warehouse.username,
+                                    'warehouse_label': "%s %s" % (warehouse.first_name, warehouse.last_name),
+                                    'source_label': "%s %s" % (source_name.first_name, source_name.last_name),
                                     'Stock Transfer ID': data['order_id'],
                                     'Quantity': data['tsum'], 'Creation Date': data['date_only'].strftime("%d %b, %Y"),
                                     'DT_RowClass': 'results', 'source_wh': data['st_po__open_st__warehouse__username'],

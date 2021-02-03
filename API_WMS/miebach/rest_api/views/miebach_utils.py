4d82383251859b0111943b7551ce58730f0be47f
Naresh Reddy <naresh@mieone.com>
2021-02-03 16:21:29 +0530

    Adding report changes and closing stock upload change

--- API_WMS/miebach/rest_api/views/miebach_utils.py
+++ API_WMS/miebach/rest_api/views/miebach_utils.py
@@ -11360,6 +11360,8 @@ def get_ageing_data(search_params, user, sub_user):
         else:
             users = User.objects.none()
     user_ids = list(users.values_list('id', flat=True))
+    sku_master, sku_master_ids = get_sku_master(user_ids, sub_user, is_list = True)
+    search_parameters['sku_id__in'] = sku_master_ids
     start_index = search_params.get('start', 0)
     stop_index = start_index + search_params.get('length', 0)
     lis = ['sku_id__wms_code', 'sku_id__sku_desc',
@@ -11390,13 +11392,14 @@ def get_ageing_data(search_params, user, sub_user):
 
     grn_numbers, sku_codes = [], []
     for model_dat in master_data:
-        grn_numbers.append(model_dat.grn_number)
+        if not model_dat.grn_number:
+            grn_numbers.append(model_dat.grn_number)
         sku_codes.append(model_dat.sku.sku_code)
     grn_model_data={}
-    seller_summary_objs= SellerPOSummary.objects.filter(
-                                Q(purchase_order__open_po__sku__sku_code__in=sku_codes) | Q(purchase_order__stpurchaseorder__open_st__sku__sku_code__in=sku_codes),
-                                grn_number__in=grn_numbers
-                            ).\
+    po_sps = list(SellerPOSummary.objects.filter(purchase_order__open_po__sku__sku_code__in=sku_codes, grn_number__in=grn_numbers).values_list('id', flat=True))
+    st_sps = list(SellerPOSummary.objects.filter(purchase_order__stpurchaseorder__open_st__sku__sku_code__in=sku_codes, grn_number__in=grn_numbers).values_list('id', flat=True))
+    po_sps.extend(st_sps)
+    seller_summary_objs= SellerPOSummary.objects.filter(id__in=po_sps).\
                         values("grn_number", "creation_date", "purchase_order__open_po__sku__sku_code", "purchase_order__stpurchaseorder__open_st__sku__sku_code")
     for grn_dat in seller_summary_objs:
         sku_code = grn_dat['purchase_order__open_po__sku__sku_code']
@@ -11406,6 +11409,7 @@ def get_ageing_data(search_params, user, sub_user):
         grn_model_data.setdefault(grn_sku_key, {"creation_date": grn_dat["creation_date"]})
 
     for data in master_data:
+        counter += 1
         grn_sku_key= (data.grn_number, data.sku.sku_code)
         _date = get_local_date(user, data.receipt_date, True)
         _date = _date.strftime("%d %b, %Y")
@@ -15979,7 +15983,7 @@ def get_cancel_grn_report_data(search_params, user, sub_user):
     from miebach_admin.models import *
     from miebach_admin.views import *
     from rest_api.views.common import get_sku_master,get_warehouse_user_from_sub_user, get_warehouses_data,get_admin, get_sub_users, \
-        get_plant_and_department
+        get_plant_and_department, check_and_get_plants_depts_wo_request
     temp_data = copy.deepcopy(AJAX_DATA)
     search_parameters = {}
 
@@ -15992,6 +15996,7 @@ def get_cancel_grn_report_data(search_params, user, sub_user):
     # search_parameters['quantity__gt'] = 0
     # search_parameters['status'] = 1
 
+    dept_mapping = copy.deepcopy(DEPARTMENT_TYPES_MAPPING)
     col_num = search_params.get('order_index', 0)
     order_term = search_params.get('order_term')
     order_data = lis[col_num]
@@ -16017,8 +16022,17 @@ def get_cancel_grn_report_data(search_params, user, sub_user):
     if 'sku_code' in search_params:
         search_parameters['purchase_order__open_po__sku__wms_code'] = search_params['sku_code']
 
-    if user.userprofile.warehouse_type not in ['ADMIN', 'DEPT', 'STORE', 'ST_HUB']:
-       search_parameters['purchase_order__open_po__sku__user'] = user.id
+    #if user.userprofile.warehouse_type not in ['ADMIN', 'DEPT', 'STORE', 'ST_HUB']:
+    #   search_parameters['purchase_order__open_po__sku__user'] = user.id
+    users = [user.id]
+    if sub_user.is_staff and user.userprofile.warehouse_type == 'ADMIN':
+        users = get_related_users_filters(user.id)
+    else:
+        users = [user.id]
+        users = check_and_get_plants_depts_wo_request(sub_user, user, users)
+    user_ids = list(users.values_list('id', flat=True))
+    search_parameters['purchase_order__open_po__sku__user__in'] = user_ids
+
 
     start_index = search_params.get('start', 0)
     stop_index = start_index + search_params.get('length', 0)
@@ -16047,7 +16061,7 @@ def get_cancel_grn_report_data(search_params, user, sub_user):
         results = model_data
     count = 0
     for result in results:
-        final_challan_date, challan_number = '', ''
+        final_challan_date, challan_number, final_invoice_date = '', '', ''
         mhl_challan_number, final_mhl_challan_date = '', ''
         grn_number = result['grn_number']
         grn_date = get_local_date(user, result['creation_date'])
@@ -16099,7 +16113,9 @@ def get_cancel_grn_report_data(search_params, user, sub_user):
             pr_raised_user = pr_data['pending_po__pending_prs__requested_user__first_name']
             requested_user = pr_data['pending_po__pending_prs__requested_user']
             req_user = User.objects.filter(id=pr_data['pending_po__pending_prs__wh_user__id'])[0]
-            pr_department, pr_plant = get_plant_and_department(req_user)
+            #pr_department, pr_plant = get_plant_and_department(req_user)
+            pr_plant = get_admin(req_user).userprofile.stockone_code
+            pr_department = dept_mapping.get(req_user.userprofile.stockone_code, '')
             pr_date = pr_data['pending_po__pending_prs__creation_date']
             category = pr_data['pending_po__sku_category']
             if pr_date:
@@ -16137,7 +16153,8 @@ def get_cancel_grn_report_data(search_params, user, sub_user):
 def get_sku_wise_cancel_grn_report_data(search_params, user, sub_user):
     from miebach_admin.models import *
     from miebach_admin.views import *
-    from rest_api.views.common import get_sku_master, get_warehouse_user_from_sub_user, get_warehouses_data,get_plant_and_department
+    from rest_api.views.common import get_sku_master, get_warehouse_user_from_sub_user, get_warehouses_data,get_plant_and_department,\
+    check_and_get_plants_depts_wo_request, get_admin
     temp_data = copy.deepcopy(AJAX_DATA)
     search_parameters = {}
     lis = ['creation_date', 'creation_date', 'creation_date', 'creation_date', 'creation_date', 'creation_date',
@@ -16152,6 +16169,13 @@ def get_sku_wise_cancel_grn_report_data(search_params, user, sub_user):
            'grn_number']
     search_parameters['quantity__gt'] = 0
     search_parameters['status'] = 1
+
+    users = [user.id]
+    if sub_user.is_staff and user.userprofile.warehouse_type == 'ADMIN':
+        users = get_related_users_filters(user.id)
+    else:
+        users = [user.id]
+        users = check_and_get_plants_depts_wo_request(sub_user, user, users)
 
     col_num = search_params.get('order_index', 0)
     order_term = search_params.get('order_term')
@@ -16179,6 +16203,9 @@ def get_sku_wise_cancel_grn_report_data(search_params, user, sub_user):
         search_parameters['purchase_order__open_po__sku__wms_code'] = search_params['sku_code']
     start_index = search_params.get('start', 0)
     stop_index = start_index + search_params.get('length', 0)
+
+    user_ids = list(users.values_list('id', flat=True))
+    search_parameters['purchase_order__open_po__sku__user__in'] = user_ids
 
     values_list = ['grn_number', 'creation_date', 'purchase_order__po_number',
                    'purchase_order__open_po__supplier__supplier_id', 'purchase_order__open_po__supplier__name',
@@ -16268,7 +16295,9 @@ def get_sku_wise_cancel_grn_report_data(search_params, user, sub_user):
             requested_user = pr_data['pending_po__pending_prs__requested_user']
             pr_plant, pr_department = '', ''
             req_user = User.objects.filter(id=pr_data['pending_po__pending_prs__wh_user__id'])[0]
-            pr_department, pr_plant = get_plant_and_department(req_user)
+            #pr_department, pr_plant = get_plant_and_department(req_user)
+            pr_plant = get_admin(req_user).userprofile.stockone_code
+            pr_department = dept_mapping.get(req_user.userprofile.stockone_code, '')
             pr_date = pr_data['pending_po__pending_prs__creation_date']
             category = pr_data['pending_po__sku_category']
             if pr_date:

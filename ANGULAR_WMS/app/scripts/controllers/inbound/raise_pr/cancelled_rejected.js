3b5e4dc336f4eaa0ac075094e571b8a74c9a0a82
mkaladhar <kaladhar@mieone.com>
2021-02-11 12:00:15 +0530

    seperation of cancelled PRs

--- ANGULAR_WMS/app/scripts/controllers/inbound/raise_pr/cancelled_rejected.js
+++ ANGULAR_WMS/app/scripts/controllers/inbound/raise_pr/cancelled_rejected.js
@@ -37,21 +37,8 @@ function ServerSideProcessingCtrl($scope, $http, $state, $timeout, Session, DTOp
               }
            })
        .withDataProp('data')
-       .withOption('drawCallback', function(settings) {
-         vm.service.make_selected(settings, vm.selected);
-         $scope.$apply(function() {vm.bt_disable = true;vm.selectAll = false;});
-       })
        .withOption('processing', true)
        .withOption('serverSide', true)
-       .withOption('createdRow', function(row, data, dataIndex) {
-            $compile(angular.element(row).contents())($scope);
-        })
-        .withOption('headerCallback', function(header) {
-            if (!vm.headerCompiled) {
-                vm.headerCompiled = true;
-                $compile(angular.element(header).contents())($scope);
-            }
-        })
        .withPaginationType('full_numbers')
        .withOption('order', [0, 'desc'])
        .withOption('rowCallback', rowCallback)
@@ -64,23 +51,6 @@ function ServerSideProcessingCtrl($scope, $http, $state, $timeout, Session, DTOp
                     "PR Raise By",  "Validation Status", "Pending Level",
                     "To Be Approved By", "Last Updated By", "Last Updated At", "Remarks"];
     vm.dtColumns = vm.service.build_colums(columns);
-    // vm.dtColumns.unshift(DTColumnBuilder.newColumn(null).withTitle(vm.service.titleHtml).notSortable().withOption('width', '20px')
-    //             .renderWith(function(data, type, full, meta) {
-    //               if( 1 == vm.dtInstance.DataTable.context[0].aoData.length) {
-    //                 vm.selected = {};
-    //               }
-    //               vm.selected[meta.row] = false;
-    //               return vm.service.frontHtml + meta.row + vm.service.endHtml;
-    //             }))
-
-    // vm.dtInstance = {};
-
-    // $scope.$on('change_filters_data', function(){
-    //   if($("#"+vm.dtInstance.id+":visible").length != 0) {
-    //     vm.dtInstance.DataTable.context[0].ajax.data[colFilters.label] = colFilters.value;
-    //     vm.service.refresh(vm.dtInstance);
-    //   }
-    // });
     vm.dtInstance = {};
     vm.model_data = {};
 
@@ -103,27 +73,6 @@ function ServerSideProcessingCtrl($scope, $http, $state, $timeout, Session, DTOp
       });
       return nRow;
     }
-    $scope.getkeys = function (event) {
-        let key = event.keyCode;
-        if (event.altKey && event.which == 78) { // alt + n  enter key
-          let index= (vm.model_data.data.length)-1
-          vm.update_data(index, true, true)
-          $('input[name="wms_code"]').trigger('focus');
-        }
-    }
-    $(document).on('keydown', 'input.detectTab', function(e) {
-      var keyCode = e.keyCode || e.which;
-
-      var fields_count = (this.closest('#tab_count').childElementCount-1);
-      var cur_td_index = (this.parentElement.nextElementSibling.cellIndex);
-      var sku_index = (this.parentNode.nextElementSibling.children[0].value);
-
-
-      if ((keyCode == 9) && (fields_count === cur_td_index)) {
-        e.preventDefault();
-        vm.update_data(Number(sku_index), false);
-      }
-    });
     vm.loadjs = function () {
       vm.rejectedCancelledCtrl = true;
       vm.service.refresh(vm.dtInstance);

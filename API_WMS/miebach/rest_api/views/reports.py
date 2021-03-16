16764920dbd2a3bf7f64e7fbdcf0d1db5e7d99ed
AvinashMieone <avinash@mieone.com>
2021-03-16 11:13:35 +0530

    #5439 consumption data report

--- API_WMS/miebach/rest_api/views/reports.py
+++ API_WMS/miebach/rest_api/views/reports.py
@@ -2893,6 +2893,15 @@ def get_sku_wise_consumption_report(request, user=''):
 
     return HttpResponse(json.dumps(temp_data), content_type='application/json')
 
+@csrf_exempt
+@login_required
+@get_admin_user
+def get_consumption_data(request, user=''):
+    headers, search_params, filter_params = get_search_params(request)
+    temp_data = get_consumption_data_(search_params, user, request.user)
+
+    return HttpResponse(json.dumps(temp_data), content_type='application/json')
+
 
 @csrf_exempt
 @login_required

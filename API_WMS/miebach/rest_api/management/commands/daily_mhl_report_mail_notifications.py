9672ff7ceb7f0cb8d318eb80d8fe3b0c704e2b46
Naresh Reddy <naresh@mieone.com>
2021-04-27 12:54:48 +0530

    #5655 updating the Full reports via cron job

--- API_WMS/miebach/rest_api/management/commands/daily_mhl_report_mail_notifications.py
+++ API_WMS/miebach/rest_api/management/commands/daily_mhl_report_mail_notifications.py
@@ -2,6 +2,8 @@ from collections import OrderedDict
 from django.core.management import BaseCommand
 from django.db.models import Q, Sum
 import os
+import tarfile
+import os.path
 import logging
 import django
 import datetime
@@ -12,6 +14,7 @@ from django.db.models import F
 from miebach_admin.models import *
 from rest_api.views.mail_server import *
 from rest_api.views.common import *
+from rest_api.views.sendgrid_mail import *
 from rest_api.views.reports import *
 
 
@@ -40,17 +43,17 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write("Script Started")
-        user = User.objects.get(username='met_admin')
+        user = User.objects.get(username='mhl_admin')
         report_file_names = []
         subject = '%s Reports dated on : %s for %s' % ('Automated Email for PR, PO, GRN ', datetime.datetime.now().date(), 'Metropolis')
         text = 'Please find the scheduled reports in the below attachments dated: %s' % str(datetime.datetime.now().date())
-        start_date = datetime.datetime.strptime('2018-01-01', '%Y-%m-%d')
+        start_date = datetime.datetime.strptime('2018-04-14', '%Y-%m-%d')
         start_date = get_utc_start_date(start_date)
         today = datetime.datetime.now()
         search_params = {'from_date': start_date, 'order_term': 'asc'}
 
         # SKU Wise PR Report
-        try:
+        '''try:
             pr_grn = get_pr_detail_report_data(search_params, user, user)
             if pr_grn:
                 excel_name = 'PR_SKU_Report'
@@ -79,10 +82,11 @@ class Command(BaseCommand):
                     excel_name += '.csv'
                 report_file_names.append({'name': excel_name, 'path': excel_path})
         except Exception as e:
-            pass
+            pass'''
 
         # PR PO GRN Report
         try:
+            search_params['pr_from_date'] = search_params['from_date']
             pr_po_grn = get_pr_po_grn_filter_data('', search_params, user, user)
             if pr_po_grn:
                 excel_name = 'PR_PO_GRN_Report'
@@ -94,8 +98,21 @@ class Command(BaseCommand):
                 else:
                     excel_name += '.csv'
                 report_file_names.append({'name': excel_name, 'path': excel_path})
+                print report_file_names
         except Exception as e:
             pass
-
-        send_mail_attachment(['kaladhar@mieone.com', 'nagi@mieone.com', 'karthik@mieone.com', 'imran@mieone.com'], subject, text, files=report_file_names)
+        #import pdb; pdb.set_trace()
+        #report_file_names.append({'path': 'static/excel_files/mhl_adminPR_PO_GRN_Report.csv', 'name': 'PR_PO_GRN_Report.csv'})
+        new_paths = []
+        zip_subdir = ""
+        for dat in report_file_names:
+            print dat
+            zip_filename ='static/excel_files/'+dat['name'] + '.tar.gz'
+            with tarfile.open(zip_filename, "w:gz") as tar:
+                tar.add(dat['path'], arcname=os.path.basename(dat['path']))
+            new_paths.append({'path': zip_filename, 'name': zip_filename.split('/')[-1]})
+        print new_paths
+        #report_file_names.append({'path': 'static/excel_files/mhl_adminPR_PO_GRN_Report.csv', 'name': 'PR_PO_GRN_Report.csv'})
+        #send_sendgrid_mail('mhl_mail@stockone.in', ['kaladhar@mieone.com'], subject, text, files=report_file_names)
+        send_mail_attachment(['kaladhar@mieone.com', 'nagi@mieone.com'], subject, text, files=new_paths)
         self.stdout.write("Updating Completed")
